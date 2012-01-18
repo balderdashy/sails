6cd110c189a519be13e903c81cc5800d69e90a87
Mike McNeil <michael.r.mcneil@gmail.com>
2012-01-17 22:12:00 -0600

    ?

--- server/app.js
+++ server/app.js
@@ -31,5 +31,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(4000);
+//console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
