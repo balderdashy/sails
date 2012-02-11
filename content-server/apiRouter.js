bfefd2d46d171ff47ccdd9815203bcaac0529bde
mikermcneil <mike@unwirednation.com>
2012-02-11 12:22:55 -0600

    Added back in correct apiRouter.

---
+++ content-server/apiRouter.js
@@ -0,0 +1,71 @@
+var ApiService = require('./services/ApiService');
+var _ = require('underscore');
+
+// Set up routing table
+exports.mapUrls = function mapUrls (app) {
+
+    /**
+     * Respond with content for a load context request
+     * (update client cache)
+     */
+	function loadRequest(req, res) {
+
+        // Get context based on request
+        var context = ApiService.getContext(req);
+
+        // Look up content schema for this context
+        ApiService.getContentSchema(context, function (content){
+            console.log("Answered read request.",content);
+
+            // Return that information to crud client
+            ApiService.respond(content,req,res);
+        });
+
+    }
+    app.get("/load*",loadRequest);
+	app.get("/content/load*",loadRequest);
+
+
+    /**
+     * Respond to read request for a specific node
+     */
+	function readRequest(req, res) {
+
+        // Get context based on request
+        var context = ApiService.getContext(req);
+
+        // Look up content schema for this context
+        ApiService.getNode(context, function (content) {
+            console.log("Answered read request.",content);
+
+            // Return that infomration to crud client
+            ApiService.respond(content,req,res);
+        });
+
+    }
+    app.get("/read*", readRequest);
+    app.get("/content/read*", readRequest);
+	
+	
+	/**
+     * Fetch paginated/filtered list of content nodes for use in CMS
+     */
+    function fetchRequest (req, res) {
+
+        // Look up content schema for this context
+        ApiService.fetch({
+			page: req.param('page') || 0,
+			max: req.param('max') || 15,
+			offset: req.param('offset') || 0,
+			sort: req.param('sort') || 'title',
+			order: req.param('order') || 'desc'
+		}, function (content){
+            console.log("Answered fetch request.",content);
+
+            // Return that information to crud client
+            ApiService.respond(content,req,res);
+        });
+    }
+	app.get("/content/fetch*",fetchRequest);
+
+}
