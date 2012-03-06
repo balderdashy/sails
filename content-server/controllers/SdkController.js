78e86931216e58f9297dc41c6445655e7ec6cb08
mikermcneil <mike@unwirednation.com>
2012-03-06 02:50:59 -0600

    hostname is now correctly generated w/ or w/o port

--- content-server/controllers/SdkController.js
+++ content-server/controllers/SdkController.js
@@ -2,6 +2,6 @@ exports.javascript = function (req, res, next ) {
 	res.header('Content-Type', 'text/javascript');
 	res.render('crud.io.client.js',{
 		layout: false,
-		serverUrl: "http://crud.io:4000"
+		serverUrl: "http://" + req.headers.host
 	});
 }
