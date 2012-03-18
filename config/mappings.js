8d761bf21bc29e95d869ca219f3b1990d186ccac
Mike McNeil <michael.r.mcneil@gmail.com>
2012-03-18 17:51:18 -0500

    Stripped out the rest of the crud.io specific stuff

--- config/mappings.js
+++ config/mappings.js
@@ -11,23 +11,8 @@ exports.customMappings = function (controllers) {
 	
 	return {
 		
-		
-		// Public API
-		'/read*': controllers.node.readRequest
-		, '/load*': controllers.node.loadRequest
-		, '/content/fetch*': controllers.node.fetchRequest
-		, '/content/load*': controllers.node.loadRequest
-		, '/content/read*': controllers.node.readRequest
-
-		// JS Browser SDK
-		, '/crud.io.client.js': controllers.sdk.javascript
-
-		// Crud.io CMS convenience mappings
-		, '/nodes': controllers.node.index
-		, '/sitemap': controllers.page.index
-		
 		// Authentication mappings
-		, '/login' :controllers.auth.login
+		'/login' :controllers.auth.login
 		, '/logout' :controllers.auth.logout
 	}
 };
@@ -39,17 +24,9 @@ exports.customMappings = function (controllers) {
 //
 exports.authMappings = function (controllers) {
 	return {
-		// Private CMS actions
-		'/': controllers.auth.basic
-		, '/node*': controllers.auth.basic
-		, '/page*': controllers.auth.basic
-		
-		// Private (crud.io CMS) convenience mappings
-		, '/nodes': controllers.auth.basic
-		, '/sitemap': controllers.auth.basic
-		
+	
 		// Authentication mappings
-		, '/login' : controllers.auth.reverse
+		'/login' : controllers.auth.reverse
 		, '/logout' : controllers.auth.basic
 	}
 }
