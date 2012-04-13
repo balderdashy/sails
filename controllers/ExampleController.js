47393937e5f18ea0b0fa52d0190501835cdb132f
Mike McNeil <michael.r.mcneil@gmail.com>
2012-04-13 02:37:18 -0500

    Added Account model objects which can be bbuilt

--- controllers/ExampleController.js
+++ controllers/ExampleController.js
@@ -1,7 +1,27 @@
 
 exports.index = function (req, res, next ) {
-	res.render('example', {
-		title: 'example'
+	var accountDetail;
+	
+	Account.build({
+		username: 'blah',
+		password: 'password'
+	}).save().
+	success(function successCallback(savedModel) {
+//		console.log("Model saved to DB.",savedModel);
+		accountDetail = success({
+			id: savedModel.id,
+			title: savedModel.title
+		});
+		
+		// Render view
+		res.render('example', {
+			title: 'example',
+			account: accountDetail
+		});
+	}).
+	error(function errorCallback(response) {
+		console.log("Error.  Could not save model to DB.",response);
+		res.json(error(response));
 	});
 }
 
@@ -15,4 +35,21 @@ exports.detail = function (req, res, next ) {
 	res.render('example', {
 		title: 'example/detail'
 	});
+}
+
+
+
+function error (response) {
+	return {
+		success: false,
+		error: (_.isString(response)) ? {
+			message: response
+		} : response
+	};
+}
+
+function success (response) {
+	return _.extend({
+		success: true
+	}, response);
 }
