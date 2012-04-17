d165aed223b307316685e3ccb9ae70b92263fd8f
Mike McNeil <michael.r.mcneil@gmail.com>
2012-04-17 02:08:31 -0500

    Stubbed out the socket.io -> expressjs virtual route handling path.

--- router.js
+++ router.js
@@ -64,7 +64,7 @@ exports.mapUrls = function mapUrls (app) {
 	}
 		
 	// TODO: When a socket.io client connects, listen for the actions in the routing table
-//	io.sockets.on('connection', newWebsocketClientConnects);
+	io.sockets.on('connection', newWebsocketClientConnects);
 	
 	
 	// Handle all other cases (wildcard)
@@ -75,6 +75,76 @@ exports.mapUrls = function mapUrls (app) {
 		}, handleWildcardRequest);
 }
 
+
+// Convert a socket.io client event callback to ExpressJS request semantics
+function socketIOToExpress (controller,action) {	
+	var req = {},
+		res = {
+			handler: handler
+		},
+		next = function (){};
+	
+	// TODO: ACTUALLY GET A HOLD OF THE REQ/RES OBJECTS HERE
+	// or, alternatively, wrap every controller method in proprietary crd
+	// 
+	
+	console.log("****************","TRIGGERED "+handler);
+	
+	return function (req,res,next){};
+//	handler(req,res,next);
+}
+
+
+// Called when a new socket.io client connects to the server
+function newWebsocketClientConnects (socket) {
+	// Map socket.io routes
+	for (var path in urlMappings) {
+		
+		var route = urlMappings[path],
+			controller,
+			action;
+		
+		console.log("MAPPED " + path + " to ",urlMappings[path]);
+		
+		
+		
+		(function (controllerName,actionName) {
+				return function (req,res,next) {					
+					// Run access control middleware
+					accessControlMiddleware(controllerName,actionName,req,res,next);
+				}
+			})(controller.id,route.action)
+		
+		// A string means this route is a redirect
+		if (_.isString(route)) {
+
+			// TODO: redirect
+		}
+		
+		// An object means this route route maps directly to a controller
+		else {
+			controller = controllers[route.controller];
+			action = controller[route.action];	
+			
+			// Emulate express semantics
+			var expressContext = socketIOToExpress(socket);
+			
+			// Invoke access control middleware
+			var authMiddleware = function(req,res,next) {accessControlMiddleware(route.controller,route.action,req,res,next);}
+			
+			
+			// Build virtual express route
+			// TODO
+			// Combine action and auth middleware
+//			middleware(emulatedExpressContext.req,emulatedExpressContext.res,emulatedExpressContext.next)
+			var virtualRoute = function(req,res,next){};
+			
+			// Assign socket event handler
+			socket.on(path, virtualRoute);
+		}
+		
+	}
+}
 
 
 
@@ -175,39 +245,6 @@ function handleWildcardRequest (req,res,next) {
 	
 }
 
-
-
-
-// Convert a socket.io client event callback to ExpressJS request semantics
-function socketIOToExpress (handler) {	
-	var req = {},
-		res = {
-			handler: handler
-		},
-		next = function (){};
-	
-	// TODO: ACTUALLY GET A HOLD OF THE REQ/RES OBJECTS HERE
-	// or, alternatively, wrap every controller method in proprietary crd
-	// 
-	
-	handler(req,res,next);
-}
-
-
-// Called when a new socket.io client connects to the server
-function newWebsocketClientConnects (socket) {
-	// Map socket.io routes
-	for (var path in urlMappings) {
-		
-		// TODO: Auth
-		
-		
-		console.log("MAPPED " + path + " to " + urlMappings[path]);
-		
-		// Map route using emulated express request
-//		socket.on(path, socketIOToExpress(urlMappings[path]));
-	}
-}
 
 
 // Load user access control configuration file
