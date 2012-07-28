0ee6caf5e08db193948e04bfa781c63aaa3fcdff
Mike McNeil <michael.r.mcneil@gmail.com>
2012-07-28 17:32:09 -0500

    Modified structure to be more modular and support unit testing.  Still really ought to be broken up further.  This should allow for the framework to be installed w/ requireJS semantics when we have time to do that down the road.

---
+++ runTests.sh
@@ -0,0 +1,1 @@
+vows ./test/*.js --spec
---
+++ test/sanityCheck.js
@@ -0,0 +1,27 @@
+// division-by-zero-test.js
+
+var vows = require('vows'),
+    assert = require('assert');
+
+// Create a Test Suite
+vows.describe('Division by Zero').addBatch({
+    'when dividing a number by zero': {
+        topic: function () { return 42 / 0 },
+
+        'we get Infinity': function (topic) {
+            assert.equal (topic, Infinity);
+        }
+    },
+    'but when dividing zero by zero': {
+        topic: function () { return 0 / 0 },
+
+        'we get a value which': {
+            'is not a number': function (topic) {
+                assert.isNaN (topic);
+            },
+            'is not equal to itself': function (topic) {
+                assert.notEqual (topic, topic);
+            }
+        }
+    }
+}).export(module);
