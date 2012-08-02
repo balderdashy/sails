b5b190744ff079d5b8bc5623b111e370f80a5a46
Mike McNeil <michael.r.mcneil@gmail.com>
2012-08-02 04:14:28 -0500

    Completed sails 'scaffolding' script.

---
+++ bin/blueprints/controller.js
@@ -0,0 +1,8 @@
+var <%- name %>Controller = {
+	
+	// Methods
+	// ex.
+	// doStuff: function (req,res) {},
+	// someOtherMethod: function (req,res) {}
+};
+_.extend(exports,<%- name %>Controller);
--- bin/sails.js
+++ bin/sails.js
@@ -5,8 +5,8 @@ var ejs = require('ejs'),
 
 var argv = 
 	require('optimist').
-	usage('Usage: $0 generate FILETYPE [string] FILENAME [string]').
-	demand(3).
+	usage('Usage: $0 generate FILETYPE [string] (NAME [string])').
+	demand(2).
 	argv;
 
 // Locate app root
@@ -18,19 +18,35 @@ var appRoot =
 // Generate a file
 if (argv._[0] == 'generate') {
 	if (argv._[1] == 'model') { generate('/blueprints/model.js',outputPath,".js"); }
-	else if (argv._[1] == 'controller') { generate('/blueprints/controller.js',outputPath,".js"); }
-	else if (argv._[1] == 'layout') { generate('/blueprints/layout.ejs',outputPath,".ejs"); }
-	else if (argv._[1] == 'view') { generate('/blueprints/view.ejs',outputPath,".ejs"); }
-	else if (argv._[1] == 'service') { generate('/blueprints/service.js',outputPath,".js"); }
+	else if (argv._[1] == 'controller') { generate('/blueprints/controller.js',outputPath,"Controller.js"); }
+	else if (argv._[1] == 'layout') { 
+		file = fs.readFileSync(__dirname+'/blueprints/layout.ejs','utf8');
+		fs.writeFileSync(outputPath+"/layout.ejs",file);
+	}
+	else if (argv._[1] == 'view') { 
+		if (!argv._[2]) {
+			throw new Error ('No output file name specified!');
+		}
+		var entityName = argv._[2],
+		file = fs.readFileSync(__dirname+'/blueprints/view.ejs','utf8');
+		file = ejs.render(file,{name: entityName});
+		fs.writeFileSync(outputPath+"/"+entityName+".ejs",file);
+	}
 }
 
 
-// Utility class to generate a file given the blueprint and output paths
-// assumes the entity is being passed as the correct cmdline argument
+
+// Utility class to generate a file given the blueprint and output paths,
+// as well as an optional ejs render override.
 function generate (blueprintPath,outputPath,outputExtension) {
+	if (!argv._[2]) {
+		throw new Error ('No output file name specified!');
+		return;
+	}
+	
 	var entityName = capitalize(argv._[2]),
 		file = fs.readFileSync(__dirname+blueprintPath,'utf8');
-	file = ejs.render(file,{name: entityName});
+		file = ejs.render(file,{name: entityName});
 	fs.writeFileSync(outputPath+"/"+entityName+outputExtension,file);
 }
 
