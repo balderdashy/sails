40c13b0e4923834fcd9d5702d762e31b5a7eedcf
Mike McNeil <michael.r.mcneil@gmail.com>
2012-11-07 21:28:33 -0600

    Updated home.ejs and added routes blueprint.

---
+++ bin/blueprints/routes.js
@@ -0,0 +1,43 @@
+// Custom URL mappings
+// *********************
+// 
+// This table routes urls to controllers/actions.
+//
+// If the URL is not specified here, the default route for a URL is:  /:controller/:action/:id
+// where :controller, :action, and the :id request parameter are derived from the url
+//
+// If :action is not specified, Sails will redirect to the appropriate action 
+// based on the HTTP verb: (using REST/Backbone conventions)
+//
+//		GET:	/:controller/read/:id
+//		POST:	/:controller/create
+//		PUT:	/:controller/update/:id
+//		DELETE:	/:controller/destroy/:id
+//
+// If the requested controller/action doesn't exist:
+//   - if a view exists ( /views/:controller/:action.ejs ), Sails will render that view
+//   - if no view exists, but a model exists, Sails will automatically generate a 
+//       JSON API for the model which matches :controller.
+//   - if no view OR model exists, Sails will respond with a 404.
+//
+module.exports = {
+	// Default mappings
+	'/': {
+		controller: 'meta',
+		action: 'home'
+	}
+
+	// Additional routes might look like:
+	// '/someArbitraryPath': {
+	//		controller: 'someController',
+	//		action: 'someAction'
+	// }
+
+	// If you only want to set up a route for a particular HTTP method, 
+	// you can specify the HTTP verb before the path:
+	//
+	// 'post /signup': {
+	//		controller: 'auth',
+	//		action: 'signup'
+	// }
+};
