e8c5f9da2dc244f318547b1537a11abe1bcc95bf
Mike McNeil <michael.r.mcneil@gmail.com>
2012-11-07 21:39:57 -0600

    Updated update.sh script for automatic tagging of version bumps (instead of a commit.)

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ then
   exit 0
 else
   sed 's/{{sailsVersion}}/'$1'/' package.json.template > package.json
-  git commit -am "Bumped version to $1."
+  #todo: use second argument (optional) as commit message and annotated tag message
+  git tag "$1"
+#  git commit -am "Bumped version to $1."
   npm publish --force && git push
 fi
