7216b460589476e4459085f71959178552792263
Mike McNeil <michael.r.mcneil@gmail.com>
2012-11-08 18:55:04 -0600

    Removed actions with views that no longer exist from MetaController.

--- bin/blueprints/MetaController.js
+++ bin/blueprints/MetaController.js
@@ -8,24 +8,6 @@ var MetaController = {
 	home: function (req,res) {
 
 		res.view('meta/home');
-	},
-
-	error: function (req,res) {
-		res.view('500', {
-			title: 'Error (500)'
-		});
-	},
-
-	notfound: function (req,res) {
-		res.view('404', {
-			title: 'Not Found (404)'
-		});
-	},
-
-	denied: function (req,res) {
-		res.view('403', {
-			title: 'Access Denied (403)'
-		});
 	}
 };
 _.extend(exports,MetaController);
