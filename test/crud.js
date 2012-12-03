8beb0ac98c2211d29942751a61e880fec9e9b43b
Mike McNeil <michael.r.mcneil@gmail.com>
2012-12-03 04:52:47 -0600

    Spiced up unit tests.

--- test/crud.js
+++ test/crud.js
@@ -10,22 +10,36 @@ var adapters = {
 var models = {
 	user: require('./models/User.js')
 };
+require("../waterline.js") (adapters, models, function() {
 
 
-describe('adapter', function() {
-	describe('#create()', function() {
-		it('should instantiate w/o errors', function(done) {
-			var $ = new parley();
-			var outcome = $(require("../waterline.js")) (adapters,models);
-			$(done)(outcome);
+
+	describe('#creating()', function() {
+
+		it('a User named Johnny', function(done) {
+			models.user.create({
+				name: "Johnny"
+			}, done);
 		});
 
-		it('should create a model which can be fetched', function(done) {
-			require("../waterline.js")(adapters,models,function () {
-				models.user.create({
-					name: "Johnny"
-				},done);
+		it('a User named Timmy should return a generated PK', function(done) {
+			models.user.create({
+				name: "Timmy"
+			}, function(err, timmy) {
+				if(err) throw err;
+				else if(!timmy || !timmy.name || timmy.name !== "Timmy") throw "Invalid model returned.";
+				else if(!timmy.id) throw "No id returned.";
+				else done(err, timmy);
 			});
 		});
+
+		it('should mean we can fetch Johnny', function(done) {
+			models.user.create({
+				name: "Johnny"
+			}, done);
+		});
+
 	});
+
+	
 });
