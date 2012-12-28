3a785333bdfed479b6280e5062721db8deb275b2
Mike McNeil <michael.r.mcneil@gmail.com>
2012-12-27 19:19:54 -0600

    Fix in bin.

--- bin/sails.js
+++ bin/sails.js
@@ -55,7 +55,7 @@ if(argv._[0] === 'generate') {
 	// Otherwise generate a model, controller, and view directory
 	else {
 		verifyArg(1, "ERROR: Please specify the name of the entity to generate a model, controller, and view for as the second argument.");
-		sails.log.debug("Generating model, controller, and view directory for "+argv._[1]);
+		console.log("Generating model, controller, and view directory for "+argv._[1]);
 		generate('model.js', "models/", argv._[1], ".js", true);
 		generate('controller.js', "controllers/", argv._[1], "Controller.js", true);
 		generateDir("views/" + argv._[1]);
@@ -64,7 +64,7 @@ if(argv._[0] === 'generate') {
 
 // Generate an app
 else {
-	sails.log.debug("\nGenerating sails project...");
+	console.log("\nGenerating sails project...");
 	verifyArg(0, "ERROR: Please specify the name of the new directory as the first argument.");
 
 
@@ -137,7 +137,7 @@ function generateFile(blueprintPath, newPath) {
 // Generate a directory
 
 function generateDir(newPath) {
-	sails.log.debug("Generating directory "+newPath+"...");
+	console.log("Generating directory "+newPath+"...");
 	fs.mkdirSync(outputPath + "/" + (newPath || ""));
 }
 
@@ -146,7 +146,7 @@ function generateDir(newPath) {
 // as well as an optional ejs render override.
 
 function generate(blueprintPath, prefix, entity, suffix, isEntityCapitalized) {
-	sails.log.debug("Generating "+blueprintPath+" for "+entity+"...");
+	console.log("Generating "+blueprintPath+" for "+entity+"...");
 
 	if (!entity) {
 		throw new Error('No output file name specified!');
@@ -188,14 +188,14 @@ function copyFile(src, dst, cb) {
 
 function verifyArg(argNo, msg) {
 	if(!argv._[argNo]) {
-		sails.log.debug(msg);
+		console.log(msg);
 		process.exit();
 	}
 }
 
 function verifyDoesntExist(path,msg) {
 	if (fileExists(outputPath)) {
-		sails.log.debug(msg);
+		console.log(msg);
 		process.exit();
 	}
 }
