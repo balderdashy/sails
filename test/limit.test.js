f2961224629181faeee84540f9b4901ae90b1087
Mike McNeil <michael.r.mcneil@gmail.com>
2013-01-05 00:22:03 -0600

    Limit implemented, tested, works.

--- test/limit.test.js
+++ test/limit.test.js
@@ -1,3 +1,4 @@
+var _=require('underscore');
 describe('limit', function() {
 	it('normal usage should not break', function(cb) {
 		User.find({
@@ -22,6 +23,27 @@ describe('limit', function() {
 		}, {
 			limit: 10
 		}, cb);
+	});
+
+	it('it should effectively limit the number of things returned', function(cb) {
+		var testName = 'true limit test';
+		var limit = 3;
+		var users = [];
+		_.each(_.range(10),function (i) {
+			users.push({name: testName+'_user'+i, type: testName});
+		});
+
+		User.createEach(users,function (err) {
+			if (err) throw new Error(err);
+			User.findAllByType(testName, {
+				limit: limit
+			}, function (err, users) {
+				if (err) throw new Error(err);
+				else if (!users) throw new Error('Unexpected result: '+users);
+				else if (users.length !== limit) throw new Error('Improper # of users returned ('+users.length+')');
+				else cb();
+			});
+		});
 	});
 
 
