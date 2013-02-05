db5bd342ed7350d8749c006ec786d632cb93fd3c
Mike McNeil <michael.r.mcneil@gmail.com>
2013-02-04 19:12:29 -0600

    Less than tested and works.

--- test/rangeQueries.test.js
+++ test/rangeQueries.test.js
@@ -1,17 +1,36 @@
 var _ = require('underscore');
-describe('lessThan', function() {
+describe('lessThan', function(cb) {
 
 	var testName = 'lessThan test';
 
+	before(function (cb) {
+		User.createEach([{
+			age: 40,
+			type: 'rangeQuery test'
+		},
+		{
+			age: 44,
+			type: 'rangeQuery test'
+		},
+		{
+			age: 45,
+			type: 'rangeQuery test'
+		},
+		{
+			age: 46,
+			type: 'rangeQuery test'
+		}], cb);
+	});
+
 	it('should work with basic usage', function(cb) {
-		User.findAll(testName, {
+		User.findAll({
 			age: {
 				lessThan: 45
 			}
 		}, function(err, users) {
 			if(err) throw new Error(err);
 			else if(!users) throw new Error('Unexpected result: ' + users);
-			else if(users.length !== limit) throw new Error('Improper # of users returned (' + users.length + ')');
+			else if(users.length !== 2) throw new Error('Improper # of users returned (' + users.length + ')');
 			else cb();
 		});
 	});
