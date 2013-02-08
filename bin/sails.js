10a4c0330cb4e2253a08f8760892693b2af74e28
Dennis Bartlett <bartlett.dc.1@gmail.com>
2013-02-08 01:20:59 -0600

    Created an Interactive Console.  It still needs some work though.

--- bin/sails.js
+++ bin/sails.js
@@ -26,6 +26,19 @@ if (argv._.length === 0) {
 else if ( _.contains(['lift', 'raise', 'launch', 'start', 'server', 'run', 's', 'l'], argv._[0]) ) {
 	require(process.cwd()+'/app.js');
 }
+// Start this app in interactive mode
+else if ( _.contains(['console'], argv._[0]) ) {
+	require(process.cwd()+'/app.js');
+	setTimeout(function () {
+	  	console.log('Sails Console started');
+  		console.log('To exit, type ".exit"');
+	}, 1000);
+	repl = require("repl").start("Sails> ");
+	repl.on('exit', function () {
+  		console.log('Closing Console');
+  		process.exit();
+	});
+}
 // Get the sails version
 else if (argv.v || argv.version || argv._[0] === 'version') {
 	sails.log.info('v'+sails.version);
