4396a55cfbc01dfa7bae141e5d98d97bfa10f36d
Mike McNeil <michael.r.mcneil@gmail.com>
2013-02-16 01:06:56 -0600

    Added values object to model, which contains the values without model methods.

--- adapter.js
+++ adapter.js
@@ -168,6 +168,7 @@ module.exports = function(adapterDef, cb) {
 					if (!_.isObject(model)) return;
 					if (!model.save) model.save = _.bind(self.__save, self, collectionName, model);
 					if (!model.destroy) model.destroy = _.bind(self.__destroy, self, collectionName, model);
+					if (!model.values) model.values = _.omit(self,['save','destroy','values']);
 				});
 				return cb(err,set);
 			}
@@ -176,6 +177,7 @@ module.exports = function(adapterDef, cb) {
 				// Add save() method to model
 				if (!set.save) set.save = _.bind(self.__save, self, collectionName, set);
 				if (!set.destroy) set.destroy = _.bind(self.__destroy, self, collectionName, set);
+				if (!set.values) set.values = _.omit(self,['save','destroy','values']);
 				return cb(err,set);
 			}
 			else return cb(err,set);
