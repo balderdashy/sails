b82dc7e517cf7dc161958ca0161a1631a20bf72f
Mike McNeil <michael.r.mcneil@gmail.com>
2013-03-04 16:54:20 -0600

    Tests for like, contains, startsWith, endsWith queries and their case-insensitivity.

--- test/waterline/adapter/caseSensitivity.test.js
+++ test/waterline/adapter/caseSensitivity.test.js
@@ -1,9 +1,8 @@
 var _ = require('underscore');
 
-var testName = 'case sensitivity';
-describe(testName, function() {
+describe('case sensitivity', function() {
 
-
+	var testName = 'case sensitivity and findAll()';
 	before(function(cb) {
 		User.createEach([
 
@@ -52,6 +51,87 @@ describe(testName, function() {
 
 		it('should work with findAllBy*()', function(cb) {
 			User.findAllByName('thetest', User.testCount(3, cb));
+		});
+	});
+
+
+	describe('special classified queries ', function () {
+
+		var testName = 'contains case sensitivity';
+		before(function(cb) {
+			User.createEach([
+
+				// several of these exist
+				{
+					name: 'tHeTest',
+					type: testName
+				}, {
+					name: 'thetest',
+					type: testName
+				}, {
+					name: 'THETEST',
+					type: testName
+				}, 
+
+				// only one of these exists
+				{
+					name: 'tHeOtherTest',
+					type: testName
+				},
+
+				{
+					name: 'OTHER THINGS 0',
+					type: testName
+				},
+
+				{
+					name: 'OTHER THINGS 1',
+					type: testName
+				},
+
+				// ends with est
+				{
+					name: '0n3 m0r3 est',
+					type: testName
+				}
+			], cb);
+		});
+
+		it('contains should work in a case insensitive fashion by default', function(cb) {
+			User.findAll({
+				name: {
+					contains: 'hete'
+				},
+				type: testName
+			},User.testCount(3, cb));
+		});
+
+		it('startsWith should work in a case insensitive fashion by default', function(cb) {
+			User.findAll({
+				name: {
+					startsWith: 'the'
+				},
+				type: testName
+			},User.testCount(4, cb));
+		});
+
+		it('endsWith should work in a case insensitive fashion by default', function(cb) {
+			User.findAll({
+				name: {
+					endsWith: 'est'
+				},
+				type: testName
+			},User.testCount(5, cb));
+		});
+
+
+		it('like should work in a case insensitive fashion by default', function(cb) {
+			User.findAll({
+				name: {
+					like: '%hete%'
+				},
+				type: testName
+			},User.testCount(3, cb));
 		});
 	});
 });
