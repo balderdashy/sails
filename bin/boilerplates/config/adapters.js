1cc449ccb17263317d307e257ee7ed157ba09c31
Mike McNeil <michael.r.mcneil@gmail.com>
2013-03-31 00:16:10 -0500

    Default to dirty disk.  Also a fix for string ids.

--- bin/boilerplates/config/adapters.js
+++ bin/boilerplates/config/adapters.js
@@ -5,7 +5,7 @@ module.exports.adapters = {
 
 	// If you leave the adapter config unspecified 
 	// in a model definition, 'default' will be used.
-	'default': 'memory',
+	'default': 'dirty',
 	
 	// In-memory adapter for DEVELOPMENT ONLY
 	// (data is NOT preserved when the server shuts down)
