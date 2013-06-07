81e160e657024d94aa934eb666eda1b5a6138247
Mike McNeil <michael.r.mcneil@gmail.com>
2013-06-07 02:53:20 -0500

    Fixed up some asset stuff, but also ventured down what is perhaps, a dark tunnel of bad ideas.

--- bin/boilerplates/config/routes.js
+++ bin/boilerplates/config/routes.js
@@ -23,8 +23,9 @@
 module.exports.routes = {
 	
 	// To route the home page to the "index" action of FooController
+	// (if no controller exists, Sails will look for a view called `views/home/index.*`)
 	// '/' : {
-	// 	controller	: 'foo'
+	// 	controller	: 'home'
 	// }
 
 	// If you want to set up a route only for a particular HTTP method/verb 
