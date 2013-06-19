d9b6fe1fe1f472395a292e9537217e9c367d23d5
Robert Djurasaj <robert.djurasaj@gmail.com>
2013-06-19 10:53:08 -0600

    remove node-dirty note from adapters.js boilerplate

--- bin/boilerplates/config/adapters.js
+++ bin/boilerplates/config/adapters.js
@@ -16,9 +16,6 @@ module.exports.adapters = {
 
 	// Persistent adapter for DEVELOPMENT ONLY
 	// (data IS preserved when the server shuts down)
-	// PLEASE NOTE: disk adapter not compatible with node v0.10.0 currently 
-	//				because of limitations in node-dirty
-	//				See https://github.com/felixge/node-dirty/issues/34
 	disk: {
 		module: 'sails-dirty',
 		filePath: './.tmp/dirty.db',
