b819ba18ebbabb1dc2e78b6d0b6fb9ac44aeb4ea
Andrei Shchatko <andrei.shchatko@gmail.com>
2013-07-05 20:37:09 +0400

    Expanded tabs into spaces in bin/boilerplates

--- bin/boilerplates/config/adapters.js
+++ bin/boilerplates/config/adapters.js
@@ -3,28 +3,28 @@
 // it will override anything from this global config.
 module.exports.adapters = {
 
-	// If you leave the adapter config unspecified 
-	// in a model definition, 'default' will be used.
-	'default': 'disk',
-	
-	// In-memory adapter for DEVELOPMENT ONLY
-	memory: {
-		module: 'sails-memory'
-	},
+  // If you leave the adapter config unspecified 
+  // in a model definition, 'default' will be used.
+  'default': 'disk',
 
-	// Persistent adapter for DEVELOPMENT ONLY
-	// (data IS preserved when the server shuts down)
-	disk: {
-		module: 'sails-disk'
-	},
+  // In-memory adapter for DEVELOPMENT ONLY
+  memory: {
+    module: 'sails-memory'
+  },
 
-	// MySQL is the world's most popular relational database.
-	// Learn more: http://en.wikipedia.org/wiki/MySQL
-	mysql: {
-		module		: 'sails-mysql',
-		host		: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-		user		: 'YOUR_MYSQL_USER',
-		password	: 'YOUR_MYSQL_PASSWORD',
-		database	: 'YOUR_MYSQL_DB'
-	}
+  // Persistent adapter for DEVELOPMENT ONLY
+  // (data IS preserved when the server shuts down)
+  disk: {
+    module: 'sails-disk'
+  },
+
+  // MySQL is the world's most popular relational database.
+  // Learn more: http://en.wikipedia.org/wiki/MySQL
+  mysql: {
+    module    : 'sails-mysql',
+    host    : 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user    : 'YOUR_MYSQL_USER',
+    password  : 'YOUR_MYSQL_PASSWORD',
+    database  : 'YOUR_MYSQL_DB'
+  }
 };
--- bin/boilerplates/config/routes.js
+++ bin/boilerplates/config/routes.js
@@ -1,6 +1,6 @@
 // Routes
 // *********************
-// 
+//
 // This table routes urls to controllers/actions.
 //
 // If the URL is not specified here, the default route for a URL is:  /:controller/:action/:id
@@ -9,10 +9,10 @@
 // If :action is not specified, Sails will redirect to the appropriate action 
 // based on the HTTP verb: (using REST/Backbone conventions)
 //
-//		GET:	/:controller/read/:id
-//		POST:	/:controller/create
-//		PUT:	/:controller/update/:id
-//		DELETE:	/:controller/destroy/:id
+//    GET:  /:controller/read/:id
+//    POST: /:controller/create
+//    PUT:  /:controller/update/:id
+//    DELETE: /:controller/destroy/:id
 //
 // If the requested controller/action doesn't exist:
 //   - if a view exists ( /views/:controller/:action.ejs ), Sails will render that view
@@ -21,51 +21,51 @@
 //   - if no view OR model exists, Sails will respond with a 404.
 //
 module.exports.routes = {
-	
-	// To route the home page to the "index" action of FooController
-	// (if no controller exists, Sails will look for a view called `views/home/index.*`)
-	// '/' : {
-	//     controller  : 'foo',
-	//     action      : 'index',
-	// }
 
-	// If you want to set up a route only for a particular HTTP method/verb 
-	// (GET, POST, PUT, DELETE) you can specify the verb before the path:
-	// 'post /signup': {
-	//		controller	: 'user',
-	//		action		: 'signup'
-	// }
+  // To route the home page to the "index" action of FooController
+  // (if no controller exists, Sails will look for a view called `views/home/index.*`)
+  // '/' : {
+  //     controller  : 'foo',
+  //     action      : 'index',
+  // }
 
-	// Keep in mind default routes exist for each of your controllers
-	// So if you have a UserController with an action called "juggle" 
-	// a route will be automatically exist mapping it to /user/juggle.
-	//
-	// Additionally, unless you override them, new controllers will have 
-	// create(), find(), findAll(), update(), and destroy() actions, 
-	// and routes will exist for them as follows:
-	/*
+  // If you want to set up a route only for a particular HTTP method/verb 
+  // (GET, POST, PUT, DELETE) you can specify the verb before the path:
+  // 'post /signup': {
+  //    controller  : 'user',
+  //    action    : 'signup'
+  // }
 
-	// Standard RESTful routing
-	// (if index is not defined, findAll will be used)
-	'get /user': {
-		controller	: 'user',
-		action		: 'index'
-	},
-	'get /user/:id': {
-		controller	: 'user',
-		action		: 'find'
-	},
-	'post /user': {
-		controller	: 'user',
-		action		: 'create'
-	},
-	'put /user/:id': {
-		controller	: 'user',
-		action		: 'update'
-	},
-	'delete /user/:id': {
-		controller	: 'user',
-		action		: 'destroy'
-	}
-	*/
+  // Keep in mind default routes exist for each of your controllers
+  // So if you have a UserController with an action called "juggle" 
+  // a route will be automatically exist mapping it to /user/juggle.
+  //
+  // Additionally, unless you override them, new controllers will have 
+  // create(), find(), findAll(), update(), and destroy() actions, 
+  // and routes will exist for them as follows:
+  /*
+
+  // Standard RESTful routing
+  // (if index is not defined, findAll will be used)
+  'get /user': {
+    controller  : 'user',
+    action    : 'index'
+  },
+  'get /user/:id': {
+    controller  : 'user',
+    action    : 'find'
+  },
+  'post /user': {
+    controller  : 'user',
+    action    : 'create'
+  },
+  'put /user/:id': {
+    controller  : 'user',
+    action    : 'update'
+  },
+  'delete /user/:id': {
+    controller  : 'user',
+    action    : 'destroy'
+  }
+  */
 };
