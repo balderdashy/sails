cacdd65a962618d687dff0f0f3d5900537f5091f
Mike McNeil <michael.r.mcneil@gmail.com>
2013-07-08 06:03:58 -0500

    Added views config boilerplate.

---
+++ bin/boilerplates/config/views.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+	// Engine for views (can be ejs, haml, etc.)
+	engine: 'ejs',
+
+	// Automatically serve views at the appropriate route
+	// (e.g. `views/404.ejs` is served at the route `/404`)
+	blueprints: true,
+
+	// Layout is on by default, in the top level of the view directory
+	// true === use default
+	// false === don't use a layout
+	// string === path to layout
+	layout: true
+
+};
