1a960f93ccf29cf638d13beedf661cb13dff6366
Mike McNeil <michael.r.mcneil@gmail.com>
2013-07-11 03:44:22 -0500

    Enhanced request object for error handler.

--- bin/boilerplates/config/routes.js
+++ bin/boilerplates/config/routes.js
@@ -138,14 +138,14 @@ module.exports[500] = function (errors, req, res, defaultErrorBehavior) {
 	// Then log them
 	for (var i in displayedErrors) {
 		if (!displayedErrors[i] instanceof Error) {
-			displayedErrors[i] = new Error(displayedErrors[i]);
+			displayedErrors[i] = require('util').inspect(new Error(displayedErrors[i]));
 			sails.log.error(displayedErrors[i]);
 		}
 	}
 
 	// In production, don't display any identifying information about the error(s)
 	var response = {};
-	if (sails.config.environment === development) {
+	if (sails.config.environment === 'development') {
 		response = {
 			errors: displayedErrors
 		};
