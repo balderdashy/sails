84e91056b76a4065c657f8ca5b25caf43bfbaf32
Mike McNeil <michael.r.mcneil@gmail.com>
2013-07-15 14:41:02 -0500

    Fixing boilerplateissues

--- bin/boilerplates/config/routes.js
+++ bin/boilerplates/config/routes.js
@@ -1,6 +1,6 @@
 /**
  * Routes
- * 
+ *
  * Sails uses a number of different strategies to route requests.
  * Here they are top-to-bottom, in order of precedence.
  *
@@ -14,11 +14,10 @@
  * (1) Static assets
  *
  * Flat files in your `assets` directory- (these are sometimes referred to as 'public')
- * If you have an image file at `/assets/images/foo.jpg`, it will be made available 
+ * If you have an image file at `/assets/images/foo.jpg`, it will be made available
  * automatically via the route:  `/images/foo.jpg`
  *
  */
-
 
 
 
@@ -90,13 +89,12 @@ module.exports.routes = {
   // to the `/google` route to Google's website:
   'get /google': 'http://google.com'
 
-  * /
+  */
 };
 
 
 
-
-/ * * * (3) Action blueprints * * These routes can be disabled by setting( in config / controllers.js): * `module.exports.controllers.blueprints.actions = false` * * All of your controllers ' actions are automatically bound to a route.  For example:
+/** (3) Action blueprints * * These routes can be disabled by setting( in config / controllers.js): * `module.exports.controllers.blueprints.actions = false` * * All of your controllers ' actions are automatically bound to a route.  For example:
  *   + If you have a controller, `FooController`:
  *     + its action `bar` is accessible at `/foo/bar`
  *     + its action `index` is accessible at `/foo/index`, and also `/foo`
@@ -109,36 +107,36 @@ module.exports.routes = {
  * These routes can be disabled by setting (in config/controllers.js):
  *		`module.exports.views.blueprints = false`
  *
- * If you have a view file at `/views/foo/bar.ejs`, it will be rendered and served 
+ * If you have a view file at `/views/foo/bar.ejs`, it will be rendered and served
  * automatically via the route:  `/foo/bar`
  *
  */
 
 /**
  * (5) Shortcut CRUD blueprints
- * 
+ *
  * These routes can be disabled by setting (in config/controllers.js)
  *			`module.exports.controllers.blueprints.shortcuts = false`
  *
- * If you have a model, `Foo`, and a controller, `FooController`, 
+ * If you have a model, `Foo`, and a controller, `FooController`,
  * you can access CRUD operations for that model at:
  *		/foo/find/:id?	->	search lampshades using specified criteria or with id=:id
  *
  *		/foo/create		->	create a lampshade using specified values
- *				
+ *
  *		/foo/update/:id	->	update the lampshade with id=:id
- *				
+ *
  *		/foo/destroy/:id	->	delete lampshade with id=:id
- *				
+ *
  */
 
 /**
  * (6) REST blueprints
- * 
+ *
  * These routes can be disabled by setting (in config/controllers.js)
  *		`module.exports.controllers.blueprints.rest = false`
  *
- * If you have a model, `Foo`, and a controller, `FooController`, 
+ * If you have a model, `Foo`, and a controller, `FooController`,
  * you can access CRUD operations for that model at:
  *
  *		get /foo/:id?	->	search lampshades using specified criteria or with id=:id
@@ -154,5 +152,6 @@ module.exports.routes = {
 
 /**
  * Finally, if nothing else matched, the default 404 handler is triggered.
- * See `config/404.js` to adjust your app'
-  s 404 logic.*//
+ * See `config/404.js` to adjust your app's 404 logic.
+ */
+ 
