882a15b23feca1a436f821b16c94d1649700d4de
Cody Stoltman <particlebanana@gmail.com>
2013-07-18 12:20:37 -0500

    initial work with i18n support

---
+++ bin/boilerplates/config/i18n.js
@@ -0,0 +1,16 @@
+module.exports.i18n = {
+
+  // Which locales are supported?
+  locales: ['en-US'],
+
+  // What is the default locale?
+  defaultLocale: 'en-US',
+
+  // Where are your locale translations located?
+  localesDirectory: 'config/locales',
+
+  // Which format are your locales in?
+  // available: [po, plist, json]
+  translationType: 'json'
+
+};
