a5e4f8166a9a73b128d3171a182a32a9579c3824
Greg Thornton <xdissent@me.com>
2013-09-12 01:45:26 -0500

    Add json-api tests

--- test/router/integration/router.APIScaffold.test.js
+++ test/router/integration/router.APIScaffold.test.js
@@ -122,4 +122,115 @@ describe('API scaffold routes', function() {
 			});
 		});
 	});
+
+	describe('with JSON-API turned on', function() {
+
+		before(function () {
+			httpHelper.writeBlueprint({pluralize: true, jsonAPI: true});
+		});
+
+		describe('a post request to /:controller', function() {
+
+			it('should return JSON-API for a newly created instance of the test model', function(done) {
+
+				var body = JSON.stringify({empties: [{test: 555}]});
+
+				httpHelper.testRoute('post', {url: 'empties', json: true, body: body}, function(err, response) {
+					if (err) done(new Error(err));
+
+					assert(response.body instanceof Object);
+					assert(response.body.empties instanceof Array);
+					assert(response.body.empties.length === 1);
+					assert(response.body.empties[0].id === 3);
+					assert(response.body.empties[0].test === 555);
+					done();
+				});
+			});
+		});
+
+		describe('a get request to /:controller with ids query param as string', function() {
+
+			it('should return JSON-API for all instances of the test model with the given ids', function(done) {
+
+				httpHelper.testRoute('get', {url: 'empties', json: true, qs: {ids: '2,3'}}, function(err, response) {
+					if (err) done(new Error(err));
+
+					assert(response.body instanceof Object);
+					assert(response.body.empties instanceof Array);
+					assert(response.body.empties.length === 2);
+					assert(response.body.empties[0].id === 2);
+					assert(response.body.empties[1].id === 3);
+					done();
+				});
+			});
+		});
+
+		describe('a get request to /:controller with ids query param as array', function() {
+
+			it('should return JSON-API for all instances of the test model with the given ids', function(done) {
+
+				httpHelper.testRoute('get', {url: 'empties', json: true, qs: {ids: [2, 3]}}, function(err, response) {
+					if (err) done(new Error(err));
+
+					assert(response.body instanceof Object);
+					assert(response.body.empties instanceof Array);
+					assert(response.body.empties.length === 2);
+					assert(response.body.empties[0].id === 2);
+					assert(response.body.empties[1].id === 3);
+					done();
+				});
+			});
+		});
+
+		describe('a get request to /:controller/:ids', function() {
+
+			it('should return JSON-API for all instances of the test model with the given ids', function(done) {
+
+				httpHelper.testRoute('get', {url: 'empties/2,3', json: true}, function(err, response) {
+					if (err) done(new Error(err));
+
+					assert(response.body instanceof Object);
+					assert(response.body.empties instanceof Array);
+					assert(response.body.empties.length === 2);
+					assert(response.body.empties[0].id === 2);
+					assert(response.body.empties[1].id === 3);
+					done();
+				});
+			});
+		});
+
+		describe('a patch request to /:controller/:id', function() {
+
+			it('should return JSON-API for the updated instance of the test model', function(done) {
+
+				var body = JSON.stringify([{op: 'replace', path: '/empties/0/test', value: 123}]),
+					headers = {'Content-Type': 'application/json-patch+json'};
+
+				httpHelper.testRoute('patch', {url: 'empties/3', json: true, body: body, headers: headers}, function(err, response) {
+					if (err) done(new Error(err));
+
+					assert(response.body instanceof Object);
+					assert(response.body.empties instanceof Array);
+					assert(response.body.empties.length === 1);
+					assert(response.body.empties[0].id === 3);
+					assert(response.body.empties[0].test === 123);
+					done();
+				});
+			});
+		});
+
+		describe('a delete request to /:controller/:id', function() {
+
+			it('should return a 204 No Content response', function(done) {
+
+				httpHelper.testRoute('del', {url: 'empties/3', json: true}, function(err, response) {
+					if (err) done(new Error(err));
+
+					assert(response.statusCode === 204);
+					assert(!response.body);
+					done();
+				});
+			});
+		});
+	});
 });
