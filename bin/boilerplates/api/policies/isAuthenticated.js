d4fa74b2cf69c977685f088d739bbd15ca5771a0
Mike McNeil <michael.r.mcneil@gmail.com>
2013-09-29 17:16:41 -0500

    More boilerplate updates.

--- bin/boilerplates/api/policies/isAuthenticated.js
+++ bin/boilerplates/api/policies/isAuthenticated.js
@@ -1,9 +1,11 @@
 /**
- * `isAuthenticated`
+ * isAuthenticated
  *
- * Simple policy to allow any authenticated user
- * Assumes that your login action (in one of your controllers) sets:
- * `req.session.authenticated = true;`
+ * @module      :: Policy
+ * @description :: Simple policy to allow any authenticated user
+ *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`
+ * @docs        :: http://sailsjs.org/#!documentation/policies
+ *
  */
 module.exports = function(req, res, next) {
 
