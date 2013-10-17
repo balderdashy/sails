62eb3bcf0f2ba255977e66d5f867f42efe63f727
Mike McNeil <michael.r.mcneil@gmail.com>
2013-10-17 01:39:38 -0500

    SSL example in local.js config.

--- bin/boilerplates/config/local.js
+++ bin/boilerplates/config/local.js
@@ -24,6 +24,18 @@
 
 module.exports = {
 
+  // Your SSL certificate and key, if you want to be able to serve HTTP responses 
+  // over https:// and/or use websockets over the wss:// protocol
+  // (recommended for HTTP, strongly encouraged for WebSockets)
+  //
+  // In this example, we'll assume you created a folder in your project, `config/ssl`
+  // and dumped your certificate/key files there:
+  // ssl: {
+  //   ca: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl_gd_bundle.crt'),
+  //   key: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl.key'),
+  //   cert: require('fs').readFileSync(__dirname + './ssl/my_apps_ssl.crt')
+  // },
+
 
   // The `port` setting determines which TCP port your app will be deployed on
   // Ports are a transport-layer concept designed to allow many different
