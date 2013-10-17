29562efd5e31eee4d1be050111d73f6e07b22814
Mike McNeil <michael.r.mcneil@gmail.com>
2013-10-17 01:10:41 -0500

    Added boilerplate express config.

---
+++ bin/boilerplates/config/express.js
@@ -0,0 +1,67 @@
+/**
+ * Configure the undlerying Express server
+ */
+module.exports.express = {
+
+
+	// Configures the middleware function used for parsing the HTTP request body
+	// Defaults to the Formidable-based version built-in to Express/Connect
+	//
+	// To enable streaming file uploads (to disk or somewhere else)
+	// you'll want to set this option to `false` to disable the body parser.
+	//
+	// Defaults to `express.bodyParser`
+	//
+	// Example override:
+	// bodyParser: (function customBodyParser (req, res, next) {})(),
+	//
+	// Alternatively, if you're comfortable with the bleeding edge,
+	// check out: https://github.com/mikermcneil/stream-debug
+	bodyParser: express.bodyParser,
+
+
+
+	// If bodyParser doesn't understand the HTTP body request data, 
+	// run it again with an artificial header, forcing it to try and parse
+	// the request body as JSON.
+	// (this allows JSON to be used as your request data without the need to 
+	// specify a 'Content-type: application/json' header)
+	//
+	// NOTE: If using the `file-parser` above, you'll want to explicitly disable this config.
+	retryBodyParserWithJSON: true,
+
+
+
+	// Cookie parser middleware to use
+	//			(or false to disable)
+	//
+	// Defaults to `express.cookieParser`
+	//
+	// Example override:
+	// cookieParser: (function customMethodOverride (req, res, next) {})(),
+
+
+
+	// HTTP method override middleware
+	//			(or false to disable)
+	//
+	// This option allows artificial query params to be passed to trick 
+	// Express into thinking a different HTTP verb was used.
+	// Useful when supporting an API for user-agents which don't allow 
+	// PUT or DELETE requests
+	//
+	// Defaults to `express.methodOverride`
+	//
+	// Example override:
+	// methodOverride: (function customMethodOverride (req, res, next) {})()
+};
+
+
+/**
+ * HTTP cache
+ * 
+ * NOTE: This cache will only be enabled in production mode (sails.config.environment = 'production')
+ */
+module.exports.cache = {
+	maxAge: 31557600000
+};
