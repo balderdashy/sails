75edc0dc8a7f676aa28623946300bb3ccad38a20
Mike McNeil <michael.r.mcneil@gmail.com>
2013-11-02 20:47:12 -0500

    Made app.js boilerplate a bit 'smarter'

--- bin/boilerplates/app.js
+++ bin/boilerplates/app.js
@@ -7,4 +7,20 @@
  * The same command-line arguments as sails lift are also supported,
  * e.g.: `node app.js --silent --port=80 --prod`
  */
-require('sails').lift(require('optimist').argv);
+
+// Ensure a "sails" can be located:
+var sails;
+try {
+	sails = require('sails');
+}
+catch (e) {
+	console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
+	console.error('You might run `npm install sails` to install sails in this directory.');
+	console.error('');
+	console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
+	console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
+	console.error('but if it doesn\'t, the app will run with the global sails instead!');
+}
+
+// Start server
+sails.lift(require('optimist').argv);
