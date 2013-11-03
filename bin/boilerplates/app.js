1dc3302bdffa9add16feb1cef4992e363be3893b
Mike McNeil <michael.r.mcneil@gmail.com>
2013-11-02 19:29:43 -0500

    Added some documentation to boilerplate app.js file.

--- bin/boilerplates/app.js
+++ bin/boilerplates/app.js
@@ -1,2 +1,10 @@
-// Start sails and pass it command line arguments
+/**
+ * app.js
+ *
+ * Use `app.js` to run your app in environments where `sails lift` is not available.
+ * To start the server, run: `node app.js`.
+ *
+ * The same command-line arguments as sails lift are also supported,
+ * e.g.: `node app.js --silent --port=80 --prod`
+ */
 require('sails').lift(require('optimist').argv);
