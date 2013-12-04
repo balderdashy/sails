34302dfafeeda9860eb5c5fe7424df69e0c24e90
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-04 17:26:48 -0600

    Generators.

---
+++ bin/generators/file/index.js
@@ -0,0 +1,16 @@
+/**
+ * Module dependencies
+ */
+
+
+/**
+ * Generator
+ *
+ * @option {String} name
+ * @handlers ok
+ * @handlers {Error} error
+ */
+module.exports = function ( options, handlers ) {
+	return handlers.ok();
+};
+
---
+++ bin/generators/folder/index.js
@@ -0,0 +1,19 @@
+/**
+ * Module dependencies
+ */
+var fs = require('fs-extra');
+
+/**
+ * Generate a folder
+ *
+ * @option {String} path
+ *
+ * @handlers ok
+ * @handlers error
+ */
+module.exports = function ( options, handlers ) {
+	
+	// fs.mkdir()
+	return handlers.ok();
+};
+
