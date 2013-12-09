5da0a32f901befcc7f77264423a4e449b6ab5eab
Mike Mitchell <contact@mikemitchell.co.uk>
2013-12-09 17:22:35 +0000

    Fixed typo

--- bin/boilerplates/config/bootstrap.js
+++ bin/boilerplates/config/bootstrap.js
@@ -1,7 +1,7 @@
 /**
  * Bootstrap
  *
- * An asynchronous boostrap function that runs before your Sails app gets lifted.
+ * An asynchronous bootstrap function that runs before your Sails app gets lifted.
  * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
  *
  * For more information on bootstrapping your app, check out:
