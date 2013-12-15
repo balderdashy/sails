ef493a26744d8be61999fa4f84991192f5241555
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-14 23:44:36 -0600

    name gitignore as a template

--- bin/generators/app/generators/gitignore/index.js
+++ bin/generators/app/generators/gitignore/index.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	configure: function(options, sails, handlers) {
 
-		options.templateFilePath = require('path').resolve(__dirname, 'gitignore.js');
+		options.templateFilePath = require('path').resolve(__dirname, 'gitignore.template');
 		options.pathToNew = require('path').resolve(options.appPath, '.gitignore');
 
 		return handlers.success(options);
