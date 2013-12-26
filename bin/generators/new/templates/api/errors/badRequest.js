451e4669ad9676262b117aebf260d774be157d0c
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-26 17:49:57 -0600

    Added errors hook.

--- bin/generators/new/templates/api/errors/badRequest.js
+++ bin/generators/new/templates/api/errors/badRequest.js
@@ -1,5 +1,5 @@
 /**
- * Default 400 (Bad Request) handler
+ * 400 (Bad Request) handler
  *
  * Sails will automatically respond using this middleware when a blueprint is requested
  * with missing or invalid parameters
@@ -20,54 +20,55 @@
  *      since if this was triggered from an AJAX or socket request, JSON should be sent instead.
  */
 
-module.exports[400] = function badRequest(validationErrors, redirectTo, req, res) {
+module.exports[400] = function badRequest(validationErrors, redirectTo) {
+  return function (req, res) {
 
-  /*
-   * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
-   * work just like their Express equivalents to handle HTTP requests, they also simulate
-   * the same interface for receiving socket messages.
-   */
+    /*
+     * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
+     * work just like their Express equivalents to handle HTTP requests, they also simulate
+     * the same interface for receiving socket messages.
+     */
 
-  var statusCode = 400;
+    var statusCode = 400;
 
-  var result = {
-    status: statusCode
-  };
+    var result = {
+      status: statusCode
+    };
 
-  // Optional validationErrors object
-  if (validationErrors) {
-    result.validationErrors = validationErrors;
-  }
+    // Optional validationErrors object
+    if (validationErrors) {
+      result.validationErrors = validationErrors;
+    }
 
-  // For requesters expecting JSON, everything works like you would expect-- a simple JSON response
-  // indicating the 400: Bad Request status with relevant information will be returned. 
-  if (req.wantsJSON) {
+    // For requesters expecting JSON, everything works like you would expect-- a simple JSON response
+    // indicating the 400: Bad Request status with relevant information will be returned. 
+    if (req.wantsJSON) {
+      return res.json(result, result.status);
+    }
+
+    // For traditional (not-AJAX) web forms, this middleware follows best-practices
+    // for when a user submits invalid form data:
+    // i.   First, a one-time-use flash variable is populated, probably a string message or an array
+    //      of semantic validation error objects.
+    // ii.  Then the  user is redirected back to `redirectTo`, i.e. the URL where the bad request originated.
+    // iii. There, the controller and/or view might use the flash `errors` to either display a message or highlight
+    //      the invalid HTML form fields.
+    if (redirectTo) {
+
+      // Set flash message called `errors` (one-time-use in session)
+      req.flash('errors', validationErrors);
+
+      // then redirect back to the `redirectTo` URL
+      return res.redirect(redirectTo);
+    }
+
+
+    // Depending on your app's needs, you may choose to look at the Referer header here 
+    // and redirect back. Please do so at your own risk!
+    // For security reasons, Sails does not provide this affordance by default.
+    // It's safest to provide a 'redirectTo' URL and redirect there directly.
+
+    // If `redirectTo` was not specified, just respond w/ JSON
     return res.json(result, result.status);
-  }
-
-  // For traditional (not-AJAX) web forms, this middleware follows best-practices
-  // for when a user submits invalid form data:
-  // i.   First, a one-time-use flash variable is populated, probably a string message or an array
-  //      of semantic validation error objects.
-  // ii.  Then the  user is redirected back to `redirectTo`, i.e. the URL where the bad request originated.
-  // iii. There, the controller and/or view might use the flash `errors` to either display a message or highlight
-  //      the invalid HTML form fields.
-  if (redirectTo) {
-
-    // Set flash message called `errors` (one-time-use in session)
-    req.flash('errors', validationErrors);
-
-    // then redirect back to the `redirectTo` URL
-    return res.redirect(redirectTo);
-  }
-
-
-  // Depending on your app's needs, you may choose to look at the Referer header here 
-  // and redirect back. Please do so at your own risk!
-  // For security reasons, Sails does not provide this affordance by default.
-  // It's safest to provide a 'redirectTo' URL and redirect there directly.
-
-  // If `redirectTo` was not specified, just respond w/ JSON
-  return res.json(result, result.status);
-
+  };
 };
--- bin/generators/new/templates/api/errors/forbidden.js
+++ bin/generators/new/templates/api/errors/forbidden.js
@@ -1,5 +1,5 @@
 /**
- * Default 403 (Forbidden) middleware
+ * 403 (Forbidden) middleware
  *
  * This middleware can be invoked from a controller or policy:
  * res.forbidden( [message] )
@@ -10,43 +10,45 @@
  * 
  */
 
-module.exports[403] = function badRequest(message, req, res) {
+module.exports = function forbidden (message) {
 
-  /*
-   * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
-   * work just like their Express equivalents to handle HTTP requests, they also simulate
-   * the same interface for receiving socket messages.
-   */
+  return function (req, res) {
 
-  var viewFilePath = '403';
-  var statusCode = 403;
+    /*
+     * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
+     * work just like their Express equivalents to handle HTTP requests, they also simulate
+     * the same interface for receiving socket messages.
+     */
 
-  var result = {
-    status: statusCode
+    var viewFilePath = '403';
+    var statusCode = 403;
+
+    var result = {
+      status: statusCode
+    };
+
+    // Optional message
+    if (message) {
+      result.message = message;
+    }
+
+    // If the user-agent wants a JSON response, send json
+    if (req.wantsJSON) {
+      return res.json(result, result.status);
+    }
+
+    // Set status code and view locals
+    res.status(result.status);
+    for (var key in result) {
+      res.locals[key] = result[key];
+    }
+    // And render view
+    res.render(viewFilePath, result, function (err) {
+      // If the view doesn't exist, or an error occured, send json
+      if (err) { return res.json(result, result.status); }
+      
+      // Otherwise, serve the `views/403.*` page
+      res.render(viewFilePath);
+    });
   };
-
-  // Optional message
-  if (message) {
-    result.message = message;
-  }
-
-  // If the user-agent wants a JSON response, send json
-  if (req.wantsJSON) {
-    return res.json(result, result.status);
-  }
-
-  // Set status code and view locals
-  res.status(result.status);
-  for (var key in result) {
-    res.locals[key] = result[key];
-  }
-  // And render view
-  res.render(viewFilePath, result, function (err) {
-    // If the view doesn't exist, or an error occured, send json
-    if (err) { return res.json(result, result.status); }
-    
-    // Otherwise, serve the `views/403.*` page
-    res.render(viewFilePath);
-  });
-
 };
