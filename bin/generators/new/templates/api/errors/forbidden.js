56513267598943ff1ae0ab516933e7622067014f
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-27 16:30:50 -0600

    Tweaks to default handlers- moved the deprecation warning to error hook.

--- bin/generators/new/templates/api/errors/forbidden.js
+++ bin/generators/new/templates/api/errors/forbidden.js
@@ -7,48 +7,45 @@
  *
  * @param {String|Object|Array} message
  *      optional message to inject into view locals or JSON response
- * 
+ *
  */
 
-module.exports = function forbidden (message) {
+module.exports = function forbidden(message) {
 
-  return function (req, res) {
+  // Get access to `req` and `res`
+  var req = this.req;
+  var res = this.res;
 
-    /*
-     * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
-     * work just like their Express equivalents to handle HTTP requests, they also simulate
-     * the same interface for receiving socket messages.
-     */
+  var viewFilePath = '403';
+  var statusCode = 403;
 
-    var viewFilePath = '403';
-    var statusCode = 403;
+  var result = {
+    status: statusCode
+  };
 
-    var result = {
-      status: statusCode
-    };
+  // Optional message
+  if (message) {
+    result.message = message;
+  }
 
-    // Optional message
-    if (message) {
-      result.message = message;
-    }
+  // If the user-agent wants a JSON response, send json
+  if (req.wantsJSON) {
+    return res.json(result, result.status);
+  }
 
-    // If the user-agent wants a JSON response, send json
-    if (req.wantsJSON) {
+  // Set status code and view locals
+  res.status(result.status);
+  for (var key in result) {
+    res.locals[key] = result[key];
+  }
+  // And render view
+  res.render(viewFilePath, result, function(err) {
+    // If the view doesn't exist, or an error occured, send json
+    if (err) {
       return res.json(result, result.status);
     }
 
-    // Set status code and view locals
-    res.status(result.status);
-    for (var key in result) {
-      res.locals[key] = result[key];
-    }
-    // And render view
-    res.render(viewFilePath, result, function (err) {
-      // If the view doesn't exist, or an error occured, send json
-      if (err) { return res.json(result, result.status); }
-      
-      // Otherwise, serve the `views/403.*` page
-      res.render(viewFilePath);
-    });
-  };
+    // Otherwise, serve the `views/403.*` page
+    res.render(viewFilePath);
+  });
 };
--- bin/generators/new/templates/api/errors/serverError.js
+++ bin/generators/new/templates/api/errors/serverError.js
@@ -1,7 +1,7 @@
 /**
  * 500 (Server Error) middleware
  *
- * If an error is thrown in a policy or controller, 
+ * If an error is thrown in a policy or controller,
  * Sails will respond using this default error handler
  *
  * This middleware can also be invoked manually from a controller or policy:
@@ -14,67 +14,62 @@
 
 module.exports = function serverError (errors) {
 
-  return function (req, res) {
+  // Get access to `req` and `res`
+  var req = this.req;
+  var res = this.res;
 
-    /*
-     * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
-     * work just like their Express equivalents to handle HTTP requests, they also simulate
-     * the same interface for receiving socket messages.
-     */
+  var viewFilePath = '500',
+    statusCode = 500,
+    i, errorToLog, errorToJSON;
 
-    var viewFilePath = '500',
-        statusCode = 500,
-        i, errorToLog, errorToJSON;
+  var result = {
+    status: statusCode
+  };
 
-    var result = {
-      status: statusCode
-    };
+  // Normalize a {String|Object|Error} or array of {String|Object|Error} 
+  // into an array of proper, readable {Error}
+  var errorsToDisplay = sails.util.normalizeErrors(errors);
+  for (i in errorsToDisplay) {
 
-    // Normalize a {String|Object|Error} or array of {String|Object|Error} 
-    // into an array of proper, readable {Error}
-    var errorsToDisplay = sails.util.normalizeErrors(errors);
-    for (i in errorsToDisplay) {
-
-      // Log error(s) as clean `stack`
-      // (avoids ending up with \n, etc.)
-      if ( errorsToDisplay[i].original ) {
-        errorToLog = sails.util.inspect(errorsToDisplay[i].original);
-      }
-      else {
-        errorToLog = errorsToDisplay[i].stack;
-      }
-      sails.log.error('Server Error (500)');
-      sails.log.error(errorToLog);
-
-      // Use original error if it exists
-      errorToJSON = errorsToDisplay[i].original || errorsToDisplay[i].message;
-      errorsToDisplay[i] = errorToJSON;
+    // Log error(s) as clean `stack`
+    // (avoids ending up with \n, etc.)
+    if (errorsToDisplay[i].original) {
+      errorToLog = sails.util.inspect(errorsToDisplay[i].original);
+    } else {
+      errorToLog = errorsToDisplay[i].stack;
     }
+    sails.log.error('Server Error (500)');
+    sails.log.error(errorToLog);
 
-    // Only include errors if application environment is set to 'development'
-    // In production, don't display any identifying information about the error(s)
-    if (sails.config.environment === 'development') {
-      result.errors = errorsToDisplay;
-    }
+    // Use original error if it exists
+    errorToJSON = errorsToDisplay[i].original || errorsToDisplay[i].message;
+    errorsToDisplay[i] = errorToJSON;
+  }
 
-    // If the user-agent wants JSON, respond with JSON
-    if (req.wantsJSON) {
+  // Only include errors if application environment is set to 'development'
+  // In production, don't display any identifying information about the error(s)
+  if (sails.config.environment === 'development') {
+    result.errors = errorsToDisplay;
+  }
+
+  // If the user-agent wants JSON, respond with JSON
+  if (req.wantsJSON) {
+    return res.json(result, result.status);
+  }
+
+  // Set status code and view locals
+  res.status(result.status);
+  for (var key in result) {
+    res.locals[key] = result[key];
+  }
+  // And render view
+  res.render(viewFilePath, result, function(err) {
+    // If the view doesn't exist, or an error occured, just send JSON
+    if (err) {
       return res.json(result, result.status);
     }
 
-    // Set status code and view locals
-    res.status(result.status);
-    for (var key in result) {
-      res.locals[key] = result[key];
-    }
-    // And render view
-    res.render(viewFilePath, result, function (err) {
-      // If the view doesn't exist, or an error occured, just send JSON
-      if (err) { return res.json(result, result.status); }
-      
-      // Otherwise, if it can be rendered, the `views/500.*` page is rendered
-      res.render(viewFilePath, result);
-    });
-  };
-
+    // Otherwise, if it can be rendered, the `views/500.*` page is rendered
+    res.render(viewFilePath, result);
+  });
 };
