101e2b9545b59ddea5ac7437f4c257a8b6b25ee9
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-12 16:01:15 -0600

    Extrapolated commands, added base config to command ctx.

---
+++ bin/commands/configure.js
@@ -0,0 +1,9 @@
+module.exports = function ( options ) {
+	var log = this._log;
+
+	log.info('To configure the Sails command-line interface, create a `.sailsrc` file.');
+	console.log();
+	log.warn('The `.sailrc` specification is currently an experimental feature.');
+	log.warn('Please share your feedback on Github! (http://github.com/balderdashy/sails)');
+	return;
+};
