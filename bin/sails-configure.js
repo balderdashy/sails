f7ff8e3350764e19a4319f63c81b441bd3495363
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-16 15:16:23 -0600

    logger tweaks, update deps

--- bin/sails-configure.js
+++ bin/sails-configure.js
@@ -12,10 +12,9 @@ var captains = require('captains-log');
 module.exports = function ( ) {
 
 	var config = {};
-	var log = captains(config.log);
+	var log = captains();
 
-	log.info('To configure the Sails command-line interface, create a `.sailsrc` file.');
-	console.log();
+	log.info('To configure the Sails command-line interface, create `~/.sailsrc`');
 	log.warn('The `.sailrc` specification is currently an experimental feature.');
 	log.warn('Please share your feedback on Github! (http://github.com/balderdashy/sails)');
 	return;
--- bin/sails-console.js
+++ bin/sails-console.js
@@ -26,21 +26,27 @@ module.exports = function () {
 	var log = captains(config.log);
 
 	console.log();
-	log.info('Welcome to the Sails console.');
-	log.info('( to exit, type <CTRL>+<C> )');
 	log.verbose('Lifting `' + process.cwd() + '` in interactive mode...');
 
 	// Now load up sails for real
 	var sails = new Sails();
-	sails.lift(_.merge({},
-		config, {
+	sails.lift(_.merge({}, config, {
 
-			// Disable ASCII ship to keep from dirtying things up
-			log: {
-				noShip: true
-			}
-		}), function(err) {
+		// Silence annoying warning
+		// (if you're in the REPL, you already know.)
+		express: {
+			silenceMultipartWarning: true
+		},
+
+		// Disable ASCII ship to keep from dirtying things up
+		log: {
+			noShip: true
+		}
+	}), function(err) {
 		if (err) return Err.fatal.failedToLoadSails(err);
+
+		log.info('Welcome to the Sails console.');
+		log.info('( to exit, type <CTRL>+<C> )');
 
 		var repl = REPL.start('sails> ');
 		repl.on('exit', function(err) {
