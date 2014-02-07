c9c1160f7a29627860cd4b9410b7eab5b722269a
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-06 18:15:56 -0600

    Added nice log message

--- bin/sails-generate.js
+++ bin/sails-generate.js
@@ -55,47 +55,19 @@ module.exports = function ( ) {
 	// Set the "invalid" exit to forward to "error"
 	cb.invalid = 'error';
 
-	// If the generator type is "api", run both the controller and model generators
-	// and add in a couple extra log messages.
-	if (scope.generatorType == 'api') {
-
-		// 'api' only takes one argument, so ignore the rest
-		cliArguments = [cliArguments[0]];
-
-		// Create the controller and model
-		async.parallel(_.map(['controller', 'model'], subGen), function(err) {
-			// As long as there were no errors, add some logs about how to call the new API
-			if (!err) {
-				cb.log.info('REST API will be available next time you lift the server.');
-				cb.log.info('(@ `/' + cliArguments[0] + '` with default settings)');
-			}
-		});
-
-		// Create a function suitable for use with async.parallel to run a generator.
-		// Uses "cb.log" b/c it has that nice log format.
-		function subGen(generator) {
-			var _scope = _.extend(_.cloneDeep(scope), {generatorType: generator});
-			return function(_cb) {
-				sailsgen (_scope, {
-					success: function() {
-						cb.log.info('Generated a new '+_scope.generatorType+' `'+_scope.id+'` at '+_scope.destDir+_scope.globalID+'.js!');
-						return _cb();
-					},
-					error: function(err) {
-						cb.error(err);
-						return _cb(err);
-					}
-				});
-			}
-		}
-
+	// If the generator type is "api", we currently treat it as a special case.
+	// (todo: pull this out into a simple generator)
+	if (scope.generatorType === 'api') {
+		require('./_generate-api')(scope, cb);
 	}
 
 	// Otherwise just run whichever generator was requested.
 	else {
 		cb.success = function() {
 			cb.log.info('Generated a new '+scope.generatorType+' `'+scope.id+'` at '+scope.destDir+scope.globalID+'.js!');
-		}
+		};
+
+		// 
 		return sailsgen( scope, cb );
 	}
 
