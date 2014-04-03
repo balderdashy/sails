bb6f388a744a8f6b88688f35470f6634516a0f0e
Mike McNeil <mike@balderdash.co>
2014-04-03 08:27:48 -0500

    Added stub for interpreter tests (ie tool we can use to automatically build the support matrices)

---
+++ test/integration/hook.sockets.interpreter.test.js
@@ -0,0 +1,58 @@
+/**
+ * Test dependencies
+ */
+
+var assert = require('assert');
+var socketHelper = require('./helpers/socketHelper.js');
+var appHelper = require('./helpers/appHelper');
+var util = require('util');
+
+
+
+describe('hook:sockets :: ', function() {
+
+  var sailsprocess;
+  var socket1;
+  var socket2;
+  var appName = 'testApp';
+
+  describe('interpreter', function() {
+
+    before(function(done) {
+      this.timeout(10000);
+      appHelper.buildAndLiftWithTwoSockets(appName, {
+        silly: false
+      }, function(err, sails, _socket1, _socket2) {
+        if (err) return done(err);
+        sailsprocess = sails;
+        socket1 = _socket1;
+        socket2 = _socket2;
+        done();
+      });
+    });
+
+    after(function() {
+      socket1.disconnect();
+      socket2.disconnect();
+      if (sailsprocess) {
+        sailsprocess.kill();
+      }
+      process.chdir('../');
+      appHelper.teardown();
+    });
+
+    afterEach(function(done) {
+      socket1.removeAllListeners();
+      socket2.removeAllListeners();
+      done();
+    });
+
+    describe('basic usage', function() {
+
+      it('should probably be tested using a different helper...');
+      // TODO: use new sails.io.js client to perform these tests
+      // see http://github.com/balderdashy/sails.io.js
+    });
+
+  });
+});
