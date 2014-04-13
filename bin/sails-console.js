6af02f274995f39bebc1a32f3002b3566c56bdae
Mike McNeil <mike@balderdash.co>
2014-04-13 00:32:42 -0500

    Refactoring/cleanup in http hook.

--- bin/sails-console.js
+++ bin/sails-console.js
@@ -5,14 +5,15 @@
  * Module dependencies
  */
 
-var Sails = require('../lib/app')
-	, path  = require('path')
-	, _     = require('lodash')
-	, rconf = require('../lib/configuration/rc')
-	, captains = require('captains-log')
-	, REPL		= require('repl');
-
+var Sails = require('../lib/app');
+var path = require('path');
+var _ = require('lodash');
+var rconf = require('../lib/configuration/rc');
+var captains = require('captains-log');
+var REPL = require('repl');
+var fs = require('fs');
 require('colors');
+
 
 
 /**
@@ -22,47 +23,50 @@ require('colors');
  * in our working directory.
  */
 
-module.exports = function () {
+module.exports = function() {
 
-	var log = captains(rconf.log);
+  var log = captains(rconf.log);
 
-	console.log();
-	log.info('Starting app in interactive mode...'.debug);
-	console.log();
+  console.log();
+  log.info('Starting app in interactive mode...'.debug);
+  console.log();
 
-	// Now load up sails for real
-	var sails = Sails();
-	sails.lift(_.merge({}, rconf, {
+  // Now load up sails for real
+  var sails = Sails();
+  sails.lift(_.merge({}, rconf, {
 
-		// Silence annoying warning
-		// (if you're in the REPL, you already know.)
-		express: {
-			silenceMultipartWarning: true
-		},
+    // Silence annoying warning
+    // (if you're in the REPL, you already know.)
+    express: {
+      silenceMultipartWarning: true
+    },
 
-		// Disable ASCII ship to keep from dirtying things up
-		log: {
-			noShip: true
-		}
-	}), function(err) {
-		if (err) return Err.fatal.failedToLoadSails(err);
+    // Disable ASCII ship to keep from dirtying things up
+    log: {
+      noShip: true
+    }
+  }), function(err) {
+    if (err) return Err.fatal.failedToLoadSails(err);
 
-		log.info('Welcome to the Sails console.');
-		log.info(('( to exit, type '+'<CTRL>+<C>'+' )').grey);
-		console.log();
+    log.info('Welcome to the Sails console.');
+    log.info(('( to exit, type ' + '<CTRL>+<C>' + ' )').grey);
+    console.log();
 
-		var repl = REPL.start('sails> ');
-    history(repl, path.join(sails.config.appPath, '.node_history'));
-		repl.on('exit', function(err) {
-			if (err) {
-				log.error(err);
-				process.exit(1);
-			}
-			process.exit(0);
-		});
+    var repl = REPL.start('sails> ');
+    history(repl, path.join(sails.config.paths.tmp, '.node_history'));
+    repl.on('exit', function(err) {
+      if (err) {
+        log.error(err);
+        process.exit(1);
+      }
+      process.exit(0);
+    });
 
-	});
+  });
 };
+
+
+
 
 /**
  * REPL History
@@ -70,8 +74,8 @@ module.exports = function () {
  * with the slight tweak of setting historyIndex to -1 so that
  * it works as expected.
  */
-var fs = require('fs');
-function history (repl, file) {
+
+function history(repl, file) {
 
   try {
     var stat = fs.statSync(file);
@@ -80,7 +84,8 @@ function history (repl, file) {
     repl.rli.historyIndex = -1;
   } catch (e) {}
 
-  var fd = fs.openSync(file, 'a'), reval = repl.eval;
+  var fd = fs.openSync(file, 'a'),
+    reval = repl.eval;
 
   repl.rli.addListener('line', function(code) {
     if (code && code !== '.history') {
@@ -96,8 +101,8 @@ function history (repl, file) {
   });
 
   repl.commands['.history'] = {
-    help : 'Show the history',
-    action : function() {
+    help: 'Show the history',
+    action: function() {
       var out = [];
       repl.rli.history.forEach(function(v, k) {
         out.push(v);
@@ -106,5 +111,4 @@ function history (repl, file) {
       repl.displayPrompt();
     }
   };
-};
-
+}
