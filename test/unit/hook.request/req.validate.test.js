661401c001e9b056df1029b823f41374bb073e84
Mike McNeil <mike@balderdash.co>
2014-04-22 22:37:59 -0500

    Tests are now pretty dern decent for req.validate()

--- test/unit/hook.request/req.validate.test.js
+++ test/unit/hook.request/req.validate.test.js
@@ -4,8 +4,8 @@
 
 var assert = require('assert');
 
-var $Sails = require('./helpers/sails');
-var $Router = require('./helpers/router');
+var $Sails = require('../helpers/sails');
+var $Router = require('../helpers/router');
 
 
 
@@ -42,7 +42,7 @@ describe('Request hook', function (){
       });
     });
 
-    it('should throw when required params are missing', function (done) {
+    it('should throw E_INVALID_PARAMS when required params are missing', function (done) {
       var ROUTEADDRESS = '/req_validate1';
       sails.router.bind(ROUTEADDRESS, function (req, res, next) {
         try {
@@ -51,6 +51,13 @@ describe('Request hook', function (){
           });
         }
         catch (e) {
+          assert(e.code === 'E_INVALID_PARAMS');
+          assert(e.status === 400);
+          assert(e.invalidParams);
+          assert(e.route);
+          assert(e.usage);
+          assert(e.toJSON);
+          assert(e.inspect);
           done();
         }
       })
@@ -62,6 +69,80 @@ describe('Request hook', function (){
       });
     });
 
+    it('should redirect and use req.flash to store error in session if `redirectTo` is specified', function (done) {
+      var ROUTEADDRESS = '/req_validate2';
+      var fakeSession = {};
+      sails.router.bind(ROUTEADDRESS, function (req, res, next) {
+        req.validate({
+          bar: 'string'
+        }, '/somewhereElse');
+      })
+      .emit('router:request', {
+        url: ROUTEADDRESS,
+        query: {
+          foo: 'hi'
+        },
+        session: fakeSession
+      }, {
+        redirect: function fakeRedirect (dest) {
+          assert(dest === '/somewhereElse');
+          assert(fakeSession.flash.error);
+          done();
+        }
+      });
+    });
+
+
+    it('should support nested usage', function (done) {
+      var ROUTEADDRESS = '/req_validate3';
+      sails.router.bind(ROUTEADDRESS, function (req, res, next) {
+        assert.doesNotThrow(function () {
+          req.validate({
+            foo: {
+              bar: 'string'
+            },
+            baz: {
+              bing: {
+                barge: 'boolean',
+                dingy: 'number',
+                batwell: 'boolean'
+              }
+            }
+          });
+        });
+        assert.throws(function () {
+          req.validate({
+            foo: {
+              bar: 'string'
+            },
+            baz: {
+              bing: {
+                barge: 'boolean',
+                dingy: 'string',
+                batwell: 'boolean'
+              }
+            }
+          });
+        });
+
+        done();
+      })
+      .emit('router:request', {
+        url: ROUTEADDRESS,
+        query: {
+          foo: {
+            bar: 'hi'
+          },
+          baz: {
+            bing: {
+              barge: false,
+              dingy: 131351393,
+              batwell: true
+            }
+          }
+        }
+      });
+    });
   });
 
 });
