6f2369b5e68366373d27d0a4098ea76884e0f5e2
Mike McNeil <mike@balderdash.co>
2014-04-27 23:24:51 -0500

    Adjust blueprint/controller tests to respect new findOne() action

--- test/integration/router.defaultController.test.js
+++ test/integration/router.defaultController.test.js
@@ -1,141 +1,166 @@
+/**
+ * Module dependencies
+ */
+
 var assert = require('assert');
 var httpHelper = require('./helpers/httpHelper.js');
 var appHelper = require('./helpers/appHelper');
 var util = require('util');
 
+
+
+
 /**
  * Errors
  */
+
 var Err = {
-	badResponse: function(response) {
-		return 'Wrong server response!  Response :::\n' + util.inspect(response.body);
-	}
+  badResponse: function(response) {
+    return 'Wrong server response!  Response :::\n' + util.inspect(response.body);
+  }
 };
 
+
+
+/**
+ * Tests
+ */
+
 describe('router :: ', function() {
-	describe('Default controller routing', function() {
-		var appName = 'testApp';
+  describe('Default controller routing', function() {
+    var appName = 'testApp';
 
-		before(function(done) {
-			this.timeout(5000);
-			appHelper.build(done);
-		});
+    before(function(done) {
+      this.timeout(5000);
+      appHelper.build(done);
+    });
 
-		beforeEach(function(done) {
-			appHelper.lift(function(err, sails) {
-				if (err) {throw new Error(err);}
-				sailsprocess = sails;
-				setTimeout(done, 100);
-			});
-		});
+    beforeEach(function(done) {
+      appHelper.lift(function(err, sails) {
+        if (err) {
+          throw new Error(err);
+        }
+        sailsprocess = sails;
+        setTimeout(done, 100);
+      });
+    });
 
-		afterEach(function(done) {
-			sailsprocess.kill();
-			done();
-		});
+    afterEach(function(done) {
+      sailsprocess.kill();
+      done();
+    });
 
-		after(function() {
-			// console.log('before `chdir ../`' + ', cwd was :: ' + process.cwd());
-			process.chdir('../');
-			// console.log('after `chdir ../`' + ', cwd was :: ' + process.cwd());
-			appHelper.teardown();
-		});
+    after(function() {
+      // console.log('before `chdir ../`' + ', cwd was :: ' + process.cwd());
+      process.chdir('../');
+      // console.log('after `chdir ../`' + ', cwd was :: ' + process.cwd());
+      appHelper.teardown();
+    });
 
 
-		describe('requests to :controller/:method', function() {
+    describe('requests to :controller/:method', function() {
 
-			it('should call the specified method of the specified controller', function(done) {
+      it('should call the specified method of the specified controller', function(done) {
 
-				httpHelper.testRoute('get', 'test/index', function(err, response) {
-					if (err) return done(new Error(err));
+        httpHelper.testRoute('get', 'test/index', function(err, response) {
+          if (err) return done(new Error(err));
 
-					assert(response.body === 'index', Err.badResponse(response));
-					done();
-				});
-			});
+          assert(response.body === 'index', Err.badResponse(response));
+          done();
+        });
+      });
 
-		});
+    });
 
-		describe('REST default routes', function() {
+    describe('REST default routes', function() {
 
-			describe('a get request to /:controller', function() {
+      describe('a get request to /:controller', function() {
 
-				it('should call the controller index method', function(done) {
+        it('should call the controller index method', function(done) {
 
-					httpHelper.testRoute('get', 'test', function(err, response) {
-						if (err) return done(new Error(err));
+          httpHelper.testRoute('get', 'test', function(err, response) {
+            if (err) return done(new Error(err));
 
-						assert(response.body === 'index', Err.badResponse(response));
-						done();
-					});
-				});
-			});
+            assert(response.body === 'index', Err.badResponse(response));
+            done();
+          });
+        });
+      });
 
-			describe('a get request to /:controller/:id', function() {
+      describe('a get request to /:controller/:id', function() {
 
-				it('should call the controller find method', function(done) {
+        it('should NOT call the controller\'s `find()` method', function(done) {
 
-					httpHelper.testRoute('get', 'test/1', function(err, response) {
-						if (err) return done(new Error(err));
+          httpHelper.testRoute('get', 'test/1', function(err, response) {
+            if (err) return done(new Error(err));
 
-						assert(response.body === 'find', Err.badResponse(response));
-						done();
-					});
-				});
-			});
+            assert(response.body !== 'find', Err.badResponse(response));
+            done();
+          });
+        });
+        it('should call the controller\'s `findOne()` method', function(done) {
 
-			describe('a get request to /:controller/create', function() {
+          httpHelper.testRoute('get', 'test/1', function(err, response) {
+            if (err) return done(new Error(err));
 
-				it('should call the controller create method', function(done) {
+            assert(response.body === 'findOne', Err.badResponse(response));
+            done();
+          });
+        });
+      });
 
-					httpHelper.testRoute('get', 'test/create', function(err, response) {
-						if (err) return done(new Error(err));
+      describe('a get request to /:controller/create', function() {
 
-						assert(response.body === 'create', Err.badResponse(response));
-						done();
-					});
-				});
-			});
+        it('should call the controller create method', function(done) {
 
-			describe('a post request to /:controller/create', function() {
+          httpHelper.testRoute('get', 'test/create', function(err, response) {
+            if (err) return done(new Error(err));
 
-				it('should call the controller create method', function(done) {
+            assert(response.body === 'create', Err.badResponse(response));
+            done();
+          });
+        });
+      });
 
-					httpHelper.testRoute('post', 'test/create', function(err, response) {
-						if (err) return done(new Error(err));
+      describe('a post request to /:controller/create', function() {
 
-						assert(response.body === 'create', Err.badResponse(response));
-						done();
-					});
-				});
-			});
+        it('should call the controller create method', function(done) {
 
-			describe('a put request to /:controller/:id', function() {
+          httpHelper.testRoute('post', 'test/create', function(err, response) {
+            if (err) return done(new Error(err));
 
-				it('should call the controller update method', function(done) {
+            assert(response.body === 'create', Err.badResponse(response));
+            done();
+          });
+        });
+      });
 
-					httpHelper.testRoute('put', 'test/1', function(err, response) {
-						if (err) return done(new Error(err));
+      describe('a put request to /:controller/:id', function() {
 
-						assert(response.body === 'update', Err.badResponse(response));
-						done();
-					});
-				});
-			});
+        it('should call the controller update method', function(done) {
 
-			describe('a delete request to /:controller/:id', function() {
+          httpHelper.testRoute('put', 'test/1', function(err, response) {
+            if (err) return done(new Error(err));
 
-				it('should call the controller destroy method', function(done) {
+            assert(response.body === 'update', Err.badResponse(response));
+            done();
+          });
+        });
+      });
 
-					httpHelper.testRoute('del', 'test/1', function(err, response) {
-						if (err) return done(new Error(err));
+      describe('a delete request to /:controller/:id', function() {
 
-						assert(response.body === 'destroy', Err.badResponse(response));
-						done();
-					});
-				});
-			});
-		});
-	});
+        it('should call the controller destroy method', function(done) {
+
+          httpHelper.testRoute('del', 'test/1', function(err, response) {
+            if (err) return done(new Error(err));
+
+            assert(response.body === 'destroy', Err.badResponse(response));
+            done();
+          });
+        });
+      });
+    });
+  });
 
 });
