42ba4eea0634259d91b92a6eafc8033e7caa3ff3
Mike McNeil <mike@balderdash.co>
2014-05-05 21:02:04 -0500

    Better backwards-compatibility checks.  Should improve req.socket vs. socket negotiation in pubsub hook, but this gets it back to where it was, with support for both v0.9 and v0.10 usage (i.e. fixes tests)

--- test/integration/fixtures/sampleapp/api/controllers/UserController.js
+++ test/integration/fixtures/sampleapp/api/controllers/UserController.js
@@ -1,21 +1,36 @@
 module.exports = {
 
-	watch: function(req, res) {
-		User.watch(req);
-		res.send(200);
-	},
+  watch: function(req, res) {
+    User.watch(req);
+    res.send(200);
+  },
 
-	message: function(req, res) {
-		User.findOne({user_id:1}, function(err, user) {
-			User.message(user, {greeting: 'hello'}, req);
-			res.send(200);
-		});
-	},
+  message: function(req, res) {
+    User.findOne({
+      user_id: 1
+    }, function(err, user) {
+      if (err) return res.json(500, {
+        error: err
+      });
+      else if (!user) {
+        return res.json(404,{
+          error: 'Expected specified user (with user_id=1) to exist...'
+        });
+      } else {
+        User.message(user, {
+          greeting: 'hello'
+        }, req);
+        return res.send(200);
+      }
+    });
+  },
 
-	subscribe: function(req, res) {
-		User.subscribe(req, {user_id:req.param('id')}, req.param('context'));
-		res.send(200);
-	}
+  subscribe: function(req, res) {
+    User.subscribe(req, {
+      user_id: req.param('id')
+    }, req.param('context'));
+    res.send(200);
+  }
 
 
 };
