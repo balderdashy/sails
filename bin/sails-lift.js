5c8aae0abba5bbd80aa040cabe9c68a2932b344a
Josh Bielick <jbielick@gmail.com>
2014-05-17 15:14:45 -0400

    only fork if running from sails, not mocha process

--- bin/sails-lift.js
+++ bin/sails-lift.js
@@ -29,7 +29,7 @@ module.exports = function() {
 
   // console.time('cli_rc');
   var log = captains(rconf.log);
-  var workerCount = rconf.workers || 1;
+  var workerCount = (1 * rconf.workers) || 1;
   // console.timeEnd('cli_rc');
 
   if (cluster.isMaster) {
