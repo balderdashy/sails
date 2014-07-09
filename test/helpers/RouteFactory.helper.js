e60d2c57bd4aec8d2219aa980093914e71b17d01
Mike McNeil <mike@balderdash.co>
2014-07-08 23:35:19 -0500

    Adds some more view tests, explores adding impl of 'res' for use w/ app.request.  Should probably wait for express4 on that

---
+++ test/helpers/RouteFactory.helper.js
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+var util = require('util');
+
+
+/**
+ * @constructor
+ */
+function RouteFactory(prefix) {
+  this._prefix = prefix;
+  this._nextTestRoute = 0;
+}
+RouteFactory.prototype.next = function() {
+  this._nextTestRoute++;
+  this.current = util.format('/tests/%s%d', this._prefix ? this._prefix + '/' : '', this._nextTestRoute);
+  return this.current;
+};
+
+
+module.exports = function (){
+  return new RouteFactory();
+};
+
