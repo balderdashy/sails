6d966bfb26ff1bca125ed039ac0a210e84aec442
Aurélien Thieriot <a.thieriot@gmail.com>
2014-07-12 13:52:06 +0100

    Starting Node Inspector if available

--- bin/sails-debug.js
+++ bin/sails-debug.js
@@ -9,7 +9,7 @@ var Sails = require('../lib/app');
 var path = require('path');
 var Womb = require('child_process');
 var CaptainsLog = require('captains-log');
-
+var Fs = require('fs');
 
 /*
 
@@ -30,13 +30,30 @@ module.exports = function() {
     pathToSails = path.resolve(__dirname, './sails.js');
   }
 
-  console.log();
-  log.info('Running node-inspector on this app...');
-  log.info('If you don\'t know what to do next, type `help`');
-  log.info('Or check out the docs:');
-  log.info('http://nodejs.org/api/debugger.html');
-  console.log();
+  // If NodeInspector is available, use it
+  var pathToInspector = path.join(appPath, '/node_modules/node-inspector');
+  if (Fs.existsSync(pathToInspector)) {
+    console.log(Sails.port);
 
+    log.info('Node Inspector available. Starting it.');
+    var inspector = Womb.spawn('./node-inspector', [], {
+      cwd: path.join(appPath, '/node_modules/.bin')
+    });
+    inspector.stdout.on('data', function (data) {
+      log.info(data.toString());
+    });
+    inspector.stderr.on('err', function (err) {
+      log.error(data.toString());
+    });
+  } else {
+
+    console.log();
+    log.info('Running node-inspector on this app...');
+    log.info('If you don\'t know what to do next, type `help`');
+    log.info('Or check out the docs:');
+    log.info('http://nodejs.org/api/debugger.html');
+    console.log();
+  }
 
   log.info(('( to exit, type ' + '<CTRL>+<C>' + ' )').grey);
   console.log();
