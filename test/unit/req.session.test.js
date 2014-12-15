5e33b163f745174473537629fc0cffafc5d9029b
Mike McNeil <mike@balderdash.co>
2014-12-15 11:07:18 -0600

    Added more session tests

--- test/unit/req.session.test.js
+++ test/unit/req.session.test.js
@@ -32,17 +32,46 @@ describe('req.session (with no session hook)', function (){
 
   describe('when responding to a virtual request',function (){
 
+    var doesSessionExist;
+    var isSessionAnObject;
+    var doesTestPropertyStillExist;
 
-    it('should exist', function (done) {
-      var doesSessionExist;
-      var isSessionAnObject;
-      app.get('/dogs', function (req, res){
+    before(function setupTestRoute(){
+      app.post('/sessionTest', function (req, res){
         doesSessionExist = !!req.session;
         isSessionAnObject = _.isObject(req.session);
+        req.session.something = 'some string';
         res.send();
       });
+
+      app.get('/sessionTest', function (req, res){
+        doesSessionExist = !!req.session;
+        isSessionAnObject = _.isObject(req.session);
+        doesTestPropertyStillExist = req.session.something === 'some string';
+        res.send();
+      });
+
+    });
+
+    it('should exist', function (done) {
       app.request({
-        url: '/dogs',
+        url: '/sessionTest',
+        method: 'POST',
+        params: {},
+        headers: {}
+      }, function (err, res, body){
+        if (err) return done(err);
+        if (res.statusCode !== 200) return done(new Error('Expected 200 status code'));
+        if (!doesSessionExist) return done(new Error('req.session should exist.'));
+        if (!isSessionAnObject) return done(new Error('req.session should be an object.'));
+        console.log('res.headers',res.headers);
+        return done();
+      });
+    });
+
+    it('should persist data between requests', function (done){
+      app.request({
+        url: '/sessionTest',
         method: 'GET',
         params: {},
         headers: {}
@@ -51,9 +80,11 @@ describe('req.session (with no session hook)', function (){
         if (res.statusCode !== 200) return done(new Error('Expected 200 status code'));
         if (!doesSessionExist) return done(new Error('req.session should exist.'));
         if (!isSessionAnObject) return done(new Error('req.session should be an object.'));
+        if (!doesTestPropertyStillExist) return done(new Error('`req.session.something` should still exist for subsequent requests.'));
         return done();
       });
     });
+
   });
 
   after(function (done) {
