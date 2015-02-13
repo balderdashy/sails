bdbbad494eeda133ef7258dc1d93a93401516b05
Mike McNeil <mike@balderdash.co>
2015-02-13 13:24:44 -0800

    sails deploy hello world

---
+++ bin/sails-deploy.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+var util = require('util');
+var path = require('path');
+
+
+/**
+ * `sails deploy`
+ *
+ * Deploy the Sails app in the current directory to a hosting provider.
+ */
+
+module.exports = function() {
+
+  console.log('helo');
+};
