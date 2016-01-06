6cc4bff089c9a414734c2cf6d3ffb416353dd947
Scott Gress <scottmgress@gmail.com>
2016-01-06 12:40:51 -0600

    Add init script to tests folder, so that tests will work w/ NPM 3+

---
+++ test/init.js
@@ -0,0 +1,21 @@
+// Initialization script that runs once before any tests.
+//
+
+// Set the "SAILS_NEW_LINK" env var so that the "sails new" generator
+// always uses symlinks (rather than doing npm install) regardless of
+// which NPM version is installed.
+//
+// Traditionally, "sails new" has sped up the process of generating a
+// new Sails app by symlinking required project dependencies from the
+// Sails module's node_modules folder.  However, starting with NPM 3,
+// this shortcut will cause subsequent dependencies (installed by the
+// end-user) to fail on install, due to the new flattened node_modules
+// file structure.
+//
+// Starting with NPM 3, doing "sails new" will normally cause
+// "npm install" to run, in order for the dependencies in the new Sails
+// app to be properly flattened.  However, it takes a long time--too long
+// for tests.  Since none of the tests install separate dependencies
+// in the fixture app, we can get away with using the old symlink strategy,
+// which can be activated with a --link option, or with an environment var.
+process.env.SAILS_NEW_LINK = true;
