2086136700d74c451659f21a6d4f04b4b5ccedb3
Scott Gress <scottmgress@gmail.com>
2016-01-08 17:50:43 -0600

    Updated "lower" unit test and "lift.lower" integration tests Each one now runs 15 instances and checks for leaked event listeners

--- test/integration/lift.lower.test.js
+++ test/integration/lift.lower.test.js
@@ -1,44 +1,57 @@
 var assert = require('assert');
 var Sails = require('../../lib/app');
+var async = require('async');
 
 describe('sails being lifted and lowered (e.g in a test framework)', function() {
 
-    for (var i = 0; i < 15; ++i) {
+  it('should clean up event listeners', function (done) {
 
-        describe('Test suite ' + i, function() {
-            var sailsServer = null;
+    // Get a list of all the current listeners on the process.
+    // Note that Mocha adds some listeners, so these might not all be empty arrays!
+    var beforeListeners = {
+      sigusr2: process.listeners('SIGUSR2'),
+      sigint: process.listeners('SIGINT'),
+      sigterm: process.listeners('SIGTERM'),
+      exit: process.listeners('exit')
+    };
 
-            before(function (done) {
-                assert.doesNotThrow(function() {
-                    Sails().lift({
-                        port: 1342,
-                        environment: process.env.TEST_ENV,
-                        log: {
-                            level: 'error'
-                        },
-                        globals: false,
-                        hooks: {
-                            grunt: false,
-                        }
-                    }, function (err, sails) {
-                        sailsServer = sails;
-                        return done(err);
-                    });
-                });
-            });
-
-            after(function (done) {
-                sailsServer.lower(function (err) {
-                    sailsServer = null;
-                    done(err);
-                });
-            });
-
-            it('run a test', function (done) {
-                assert(sailsServer);
-                done();
-            });
-
+    // Lift and lower 15 Sails apps in a row, to simulate a testing environment
+    async.forEachOfSeries(Array(1), function(undef, i, cb) {
+      var sailsServer = null;
+      Sails().lift({
+        port: 1342,
+        environment: process.env.TEST_ENV,
+        log: {
+            level: 'error'
+        },
+        globals: false,
+        hooks: {
+            grunt: false,
+        }
+      }, function (err, sails) {
+        if (err) {return cb(err);}
+        setTimeout(function() {
+          sails.lower(cb);
         });
-    }
+
+      });
+
+    }, function(err) {
+      if (err) {return done(err);}
+      // Check that we have the same # of listeners as before--that is,
+      // that all listeners that were added when the apps were initialized
+      // were subsequently removed when they were lowered.
+      assert.equal(beforeListeners.sigusr2.length,
+                   process.listeners('SIGUSR2').length);
+      assert.equal(beforeListeners.sigterm.length,
+                   process.listeners('SIGTERM').length);
+      assert.equal(beforeListeners.exit.length,
+                   process.listeners('exit').length);
+      assert.equal(beforeListeners.sigint.length,
+                   process.listeners('SIGINT').length);
+      return done();
+    });
+
+  });
+
 });
--- test/unit/app.lower.test.js
+++ test/unit/app.lower.test.js
@@ -7,6 +7,8 @@ describe('app.lower', function (){
 
   it('should clean up event listeners', function (done) {
 
+    // Get a list of all the current listeners on the process.
+    // Note that Mocha adds some listeners, so these might not all be empty arrays!
     var beforeListeners = {
       sigusr2: process.listeners('SIGUSR2'),
       sigint: process.listeners('SIGINT'),
@@ -14,21 +16,29 @@ describe('app.lower', function (){
       exit: process.listeners('exit')
     };
 
-    var app = new Sails();
-    var options = {
-      globals: false,
-      log: {
-        level: 'error'
-      }
-    };
+    // Lift and lower 15 Sails apps in a row, to simulate a testing environment
+    async.eachSeries(Array(15), function(i, cb) {
+      var app = new Sails();
+      var options = {
+        globals: false,
+        log: {
+          level: 'error'
+        }
+      };
 
-    async.series([
-      function(cb) {
-        app.load(options, cb);
-      },
-      app.initialize,
-      app.lower
-    ], function (err) {
+      async.series([
+        function(cb) {
+          app.load(options, cb);
+        },
+        app.initialize,
+        app.lower
+      ], cb);
+
+    }, function(err) {
+      if (err) {return done(err);}
+      // Check that we have the same # of listeners as before--that is,
+      // that all listeners that were added when the apps were initialized
+      // were subsequently removed when they were lowered.
       assert.equal(beforeListeners.sigusr2.length,
                    process.listeners('SIGUSR2').length);
       assert.equal(beforeListeners.sigint.length,
@@ -37,7 +47,7 @@ describe('app.lower', function (){
                    process.listeners('SIGTERM').length);
       assert.equal(beforeListeners.exit.length,
                    process.listeners('exit').length);
-      done(err);
+      return done();
     });
 
   });
