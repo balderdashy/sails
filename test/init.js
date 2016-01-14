ae69442996d7e6f9a6da75e806236b81ad9eb0ab
Mike McNeil <mike@balderdash.co>
2016-01-14 05:23:21 -0600

    clarify that npm install will not necessarily be run
    I'd like to find a different way to do this that doesn't involve doing unnecessary additional installs in the local app (e.g. _copying_ deps instead of linking them)

--- test/init.js
+++ test/init.js
@@ -12,9 +12,9 @@
 // end-user) to fail on install, due to the new flattened node_modules
 // file structure.
 //
-// Starting with NPM 3, doing "sails new" will normally cause
+// Starting with NPM 3, doing "sails new" currently causes
 // "npm install" to run, in order for the dependencies in the new Sails
-// app to be properly flattened.  However, it takes a long time--too long
+// app to be properly flattened.  However, this takes a long time--too long
 // for tests.  Since none of the tests install separate dependencies
 // in the fixture app, we can get away with using the old symlink strategy,
 // which can be activated with a --link option, or with an environment var.
