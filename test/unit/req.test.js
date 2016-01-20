121f3feb8702d44420e86707ef05e3282461d136
Scott Gress <scottmgress@gmail.com>
2016-01-20 17:12:54 -0600

    Updates to Sails request factory
     -- Add req.originalUrl
     -- Compute req.path
     -- Compute req.query based on path
     -- Fix wantsJSON to allow `false` value, FWIW
    
    refs #2732

--- test/unit/req.test.js
+++ test/unit/req.test.js
@@ -15,35 +15,90 @@ var buildReq = require('root-require')('lib/router/req');
  */
 describe('Base Request (`req`)', function (){
 
-  var req;
+  describe('with empty request', function() {
+
+    var req;
 
 
-  // Mock the request object.
-  before(function (){
-    req = buildReq();
-    req.should.be.an.Object;
-    this.req = req;
+    // Mock the request object.
+    before(function (){
+      req = buildReq();
+      req.should.be.an.Object;
+      this.req = req;
+    });
+
+
+    it('.body', function () {
+      req.body.should.be.an.Object;
+      req.body.should.be.empty;
+    });
+
+    it('.params', function () {
+      req.params.should.be.an.Array;
+      req.params.should.be.empty;
+    });
+
+    it('.query', function (){
+      req.query.should.be.an.Object;
+      req.query.should.be.empty;
+    });
+
+    it('.param()', function () {
+      should(req.param('foo'))
+        .not.be.ok;
+    });
   });
 
 
-  it('.body', function () {
-    req.body.should.be.an.Object;
-    req.body.should.be.empty;
+  describe('with url /hello?abc=123&foo=bar', function() {
+
+    var req;
+
+
+    // Mock the request object.
+    before(function (){
+      req = buildReq({url: '/hello?abc=123&foo=bar'});
+      req.should.be.an.Object;
+      this.req = req;
+    });
+
+
+    it('.body', function () {
+      req.body.should.be.an.Object;
+      req.body.should.be.empty;
+    });
+
+    it('.params', function () {
+      req.params.should.be.an.Array;
+      req.params.should.be.empty;
+    });
+
+    it('.query', function (){
+      req.query.should.be.an.Object;
+      req.query.should.have.property('abc', '123');
+      req.query.should.have.property('foo', 'bar');
+    });
+
+    it('.param()', function () {
+      should(req.param('abc')).equal('123');
+      should(req.param('foo')).equal('bar');
+    });
+
+    it('.path', function() {
+      req.path.should.be.an.String;
+      req.path.should.equal('/hello');
+    });
+
+    it('.url', function() {
+      req.url.should.be.an.String;
+      req.url.should.equal('/hello?abc=123&foo=bar');
+    });
+
+    it('.originalUrl', function() {
+      req.originalUrl.should.be.an.String;
+      req.originalUrl.should.equal('/hello?abc=123&foo=bar');
+    });
+
   });
 
-  it('.params', function () {
-    req.params.should.be.an.Array;
-    req.params.should.be.empty;
-  });
-
-  it('.query', function (){
-    req.query.should.be.an.Object;
-    req.query.should.be.empty;
-  });
-
-  it('.param()', function () {
-    should(req.param('foo'))
-      .not.be.ok;
-  });
 });
-
