9dfa819f34b019d585a92d097a87e021102f6f1f
Dan Green <danon0404@gmail.com>
2016-01-21 01:57:34 +0600

    server files watching

---
+++ bin/_watcher.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+var watch = require('glob-watcher');
+var fork  = require('child_process').fork;
+
+/**
+ * _watcher.js
+ *
+ * Restarting server if server files are changed.
+ */
+
+module.exports = function() {
+
+  if (!process.connected) {
+
+    var child = fork(process.argv[1], process.argv.slice(2, process.argv.length));
+
+    watch(["api/**/*.js", "config/**/*.js", "tasks/**/*.js"], _.debounce(() => {
+      child.kill('SIGINT');
+      child = fork(process.argv[1], process.argv.slice(2, process.argv.length));
+    }), 200);
+
+    return true;
+  }
+
+  return false;
+};
