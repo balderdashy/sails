2cc910496ce7593e9c0acef5623c86f74418911f
Scott Gress <scottmgress@gmail.com>
2016-01-25 17:10:58 -0600

    Added test to make sure req.options are not sticky between requests

---
+++ test/hooks/request/req.options.sticky.test.js
@@ -0,0 +1,65 @@
+/**
+ * Module dependencies
+ */
+
+var assert = require('assert');
+
+var $Sails = require('../../helpers/sails');
+var $Router = require('../../helpers/router');
+
+
+
+
+
+describe('Request hook', function (){
+
+  var sails = $Sails.load({
+    globals: false,
+    log: {
+      level: 'silent'
+    },
+    loadHooks: [
+      'moduleloader',
+      'userconfig',
+      'request',
+      'responses'
+    ]
+  });
+
+  describe('using req.options in multiple requests', function () {
+
+    var opts;
+    before(function(done) {
+      var ROUTEADDRESS = '/route';
+      sails.router.bind(ROUTEADDRESS, function (req,res,next) {
+        req.options[req.param('opt')] = true;
+        return res.send(200, JSON.stringify(req.options));
+      });
+      sails.emit('router:request', {
+        url: ROUTEADDRESS,
+        query: {
+          opt: 'foo'
+        }
+      }, {send: function(){}});
+      sails.emit('router:request', {
+        url: ROUTEADDRESS,
+        query: {
+          opt: 'bar'
+        }
+      }, {
+        send: function (_statusCode, body) {
+          opts = JSON.parse(body);
+          return done();
+        }
+      });
+    });
+
+    it('req.options should not be sticky', function () {
+      assert(!opts.foo, "req.options.foo from first request carried over to second request!");
+    });
+
+  });
+
+
+
+});
