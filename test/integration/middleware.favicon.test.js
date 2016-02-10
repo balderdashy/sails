f6a294147f7a154dd16ed8ca771a32927f50790b
Scott Gress <scottmgress@gmail.com>
2016-02-10 15:06:43 -0600

    Add tests for favicon middleware

---
+++ test/integration/middleware.favicon.test.js
@@ -0,0 +1,94 @@
+var _ = require('lodash');
+var request = require('request');
+var Sails = require('../../lib').Sails;
+var assert = require('assert');
+var fs = require('fs-extra');
+var request = require('request');
+var appHelper = require('./helpers/appHelper');
+var path = require('path');
+
+describe('middleware :: ', function() {
+
+  describe('favicon :: ', function() {
+
+    var appName = 'testApp';
+
+    before(function(done) {
+      this.timeout(5000);
+      appHelper.build(done);
+    });
+
+    after(function() {
+      process.chdir('../');
+      // appHelper.teardown();
+    });
+
+    describe('with no favicon file in the assets folder', function() {
+
+      before(function(done) {
+        appHelper.lift(function(err, _sailsServer) {
+          assert(!err);
+          sailsServer = _sailsServer;
+          return done();
+        });
+      });
+
+      after(function(done) {
+        sailsServer.lower(done);
+      });
+
+      it('the default sailboat favicon should be provided', function(done) {
+
+        var default_favicon = fs.readFileSync(path.resolve(__dirname, '../../lib/hooks/http/public/favicon.ico'));
+        request(
+          {
+            method: 'GET',
+            uri: 'http://localhost:1342/favicon.ico',
+          },
+          function(err, response, body) {
+            assert.equal(default_favicon.toString('utf-8'), body);
+            return done();
+          }
+        );
+
+      });
+
+    });
+
+    describe('with a favicon file in the assets folder', function() {
+
+      var customFaviconPath = path.resolve(__dirname, 'fixtures/favicon.ico');
+      before(function(done) {
+        console.log(customFaviconPath);
+        fs.copySync(customFaviconPath, path.resolve('../', appName, '.tmp/public/favicon.ico'));
+        appHelper.lift(function(err, _sailsServer) {
+          assert(!err);
+          sailsServer = _sailsServer;
+          return done();
+        });
+      });
+
+      after(function(done) {
+        sailsServer.lower(done);
+      });
+
+      it('the default sailboat favicon should be provided', function(done) {
+        var custom_favicon = fs.readFileSync(customFaviconPath);
+        request(
+          {
+            method: 'GET',
+            uri: 'http://localhost:1342/favicon.ico',
+          },
+          function(err, response, body) {
+            assert.equal(custom_favicon.toString('utf-8'), body);
+            return done();
+          }
+        );
+
+      });
+
+    });
+
+  });
+
+});
