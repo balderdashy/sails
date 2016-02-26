e39e50aba23a63e48f8da8b900920eabf1c095b3
Scott Gress <scottmgress@gmail.com>
2016-02-26 19:45:55 +0100

    More reformatting

--- test/integration/router.viewRendering.test.js
+++ test/integration/router.viewRendering.test.js
@@ -3,81 +3,81 @@ var httpHelper = require('./helpers/httpHelper.js');
 var appHelper = require('./helpers/appHelper');
 
 describe('router :: ', function() {
-	describe('View routes', function() {
-		var appName = 'testApp';
+  describe('View routes', function() {
+    var appName = 'testApp';
 
-		before(function(done) {
-			appHelper.build(done);
-		});
+    before(function(done) {
+      appHelper.build(done);
+    });
 
-		beforeEach(function(done) {
-			appHelper.lift({verbose: false}, function(err, sails) {
-				if (err) {throw new Error(err);}
-				sailsprocess = sails;
-				setTimeout(done, 100);
-			});
-		});
+    beforeEach(function(done) {
+      appHelper.lift({verbose: false}, function(err, sails) {
+        if (err) {throw new Error(err);}
+        sailsprocess = sails;
+        setTimeout(done, 100);
+      });
+    });
 
-		afterEach(function(done) {
-			sailsprocess.kill(function(){setTimeout(done, 100);});
-		});
+    afterEach(function(done) {
+      sailsprocess.kill(function(){setTimeout(done, 100);});
+    });
 
-		after(function() {
-			process.chdir('../');
-			appHelper.teardown();
-		});
+    after(function() {
+      process.chdir('../');
+      appHelper.teardown();
+    });
 
-		describe('with default routing', function() {
+    describe('with default routing', function() {
 
-			it('should respond to a get request to localhost:1342 with welcome page', function(done) {
+      it('should respond to a get request to localhost:1342 with welcome page', function(done) {
 
-				httpHelper.testRoute('get', '', function(err, response) {
-					if (err) {return done(new Error(err));}
+        httpHelper.testRoute('get', '', function(err, response) {
+          if (err) {return done(new Error(err));}
 
-					assert(response.body.indexOf('not found') < 0);
-					assert(response.body.indexOf('<!-- Default home page -->') > -1);
-					done();
-				});
-			});
+          assert(response.body.indexOf('not found') < 0);
+          assert(response.body.indexOf('<!-- Default home page -->') > -1);
+          done();
+        });
+      });
 
-			it('should wrap the view in the default layout', function(done) {
+      it('should wrap the view in the default layout', function(done) {
 
-				httpHelper.testRoute('get', '', function(err, response) {
-					if (err) {return done(new Error(err));}
-					assert(response.body.indexOf('<html>') > -1);
-					done();
-				});
-			});
+        httpHelper.testRoute('get', '', function(err, response) {
+          if (err) {return done(new Error(err));}
+          assert(response.body.indexOf('<html>') > -1);
+          done();
+        });
+      });
 
-		});
+    });
 
-		describe('with no specified routing', function() {
+    describe('with no specified routing', function() {
 
-			before(function() {
-				httpHelper.writeRoutes({});
-			});
+      before(function() {
+        httpHelper.writeRoutes({});
+      });
 
-			it('should respond to get request to :controller with the template at views/:controller/index.ejs', function(done) {
+      it('should respond to get request to :controller with the template at views/:controller/index.ejs', function(done) {
 
-				// Empty router file
+        // Empty router file
 
-				httpHelper.testRoute('get', 'viewTest', function(err, response) {
-					if (err) {return done(new Error(err));}
+        httpHelper.testRoute('get', 'viewTest', function(err, response) {
+          if (err) {return done(new Error(err));}
 
-					assert(response.body.indexOf('indexView') !== -1, response.body);
-					done();
-				});
-			});
+          assert(response.body.indexOf('indexView') !== -1, response.body);
+          done();
+        });
+      });
 
-			it('should respond to get request to :controller/:action with the template at views/:controller/:action.ejs', function(done) {
+      it('should respond to get request to :controller/:action with the template at views/:controller/:action.ejs', function(done) {
 
-				httpHelper.testRoute('get', 'viewTest/create', function(err, response) {
-					if (err) {return done(new Error(err));}
+        httpHelper.testRoute('get', 'viewTest/create', function(err, response) {
+          if (err) {return done(new Error(err));}
 
-					assert(response.body.indexOf('createView') !== -1);
-					done();
-				});
-			});
+          assert(response.body.indexOf('createView') !== -1);
+          done();
+        });
+      });
 
       it('should merge config.views.locals into the view locals', function(done) {
 
@@ -99,6 +99,6 @@ describe('router :: ', function() {
       });
 
 
-		});
-	});
+    });
+  });
 });
