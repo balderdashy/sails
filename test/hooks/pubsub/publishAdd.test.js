b99d00d4a657385f62fbb37bd695ab08354151e0
Mike McNeil <mike@balderdash.co>
2016-03-12 19:57:34 -0600

    More test cleanup/diagnoses, and remove unnecessary cloneDeeps.

--- test/hooks/pubsub/publishAdd.test.js
+++ test/hooks/pubsub/publishAdd.test.js
@@ -2,6 +2,7 @@
  * Module dependencies
  */
 
+var util = require('util');
 var assert = require('assert');
 var _ = require('lodash');
 var async = require('async');
@@ -43,10 +44,14 @@ describe('Pubsub hook', function (){
         globals: false,
         port: 1535,
         models: { migrate: 'safe' },
-        log: {level: 'silent'},
+        log: {level: 'error'},
         loadHooks: ['moduleloader','userconfig','orm', 'http', 'sockets', 'pubsub'],
         routes: {
           'PUT /dock/:id/subscribe': function (req, res){
+            console.log('app.config.environment',app.config.environment);
+            console.log('process.env.NODE_ENV',process.env.NODE_ENV);
+            console.log('app.models', app.models);
+            console.log('req._sails.models', req._sails.models);
             app.models.dock.subscribe(req, req.param('id'));
             return res.send();
           },
@@ -58,7 +63,7 @@ describe('Pubsub hook', function (){
           }
         }
       }, function (err){
-        if (err) return done(err);
+        if (err) { return done(err); }
         return done();
       });
     });
@@ -87,8 +92,10 @@ describe('Pubsub hook', function (){
     before(function (done){
       async.each(_.keys(clientSocks), function (key, next){
         clientSocks[key].put('/dock/1/subscribe', function(body, jwr) {
-          if (jwr.error) return next(err);
-          next();
+          if (jwr.error) {
+            return next(new Error('Error received on jwr.  JWR:'+util.inspect(jwr, {depth: null}) ));
+          }
+          return next();
         });
       }, done);
     });
@@ -133,7 +140,7 @@ describe('Pubsub hook', function (){
         clientSocks.lenny.post('/dock/1/addVessel', {
           vessel: 47
         }, function (body, jwr){
-          if (jwr.error) return done(jwr.error);
+          if (jwr.error) { return done(jwr.error); }
           return done();
         });
       });
@@ -158,7 +165,7 @@ describe('Pubsub hook', function (){
             name: 'The Silver Goose'
           }
         }, function (body, jwr){
-          if (jwr.error) return done(jwr.error);
+          if (jwr.error) { return done(jwr.error); }
           return done();
         });
       });
@@ -184,7 +191,7 @@ describe('Pubsub hook', function (){
             name: 'The Silver Goose'
           }
         }, function (body, jwr){
-          if (jwr.error) return done(jwr.error);
+          if (jwr.error) { return done(jwr.error); }
           //
           // NOTE:
           // publishAdd does not currently throw an error, in keeping with
