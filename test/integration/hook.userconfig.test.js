8e3774bb7d541c678ba6c7bf5f3ff9e6d712bfed
Scott Gress <scottmgress@gmail.com>
2016-10-09 22:42:20 -0500

    Properly handle case of config files with the same filename in different directories.
    Refs #3846

--- test/integration/hook.userconfig.test.js
+++ test/integration/hook.userconfig.test.js
@@ -22,7 +22,8 @@ describe('hooks :: ', function() {
       appHelper.teardown();
       async.series([
         function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/abc.js'), 'module.exports = {"foo":"goo"};', cb);},
-        function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/foo/bar.js'), 'module.exports = {"foo":"bar", "abc":123};', cb);},
+        function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/foo/bar.js'), 'module.exports = {"foo":"bar", "abc":123, "betty": "boop"};', cb);},
+        function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/lara/bar.js'), 'module.exports = {"horse":"neigh", "pig": "oink", "betty": "spaghetti"};', cb);},
         function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/env/development.js'), 'module.exports = {"cat":"meow"};', cb);},
         function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/env/development/config.js'), 'module.exports = {"owl":"hoot"};', cb);},
         function(cb) {fs.outputFile(path.resolve(__dirname,'../../testApp/config/env/test-development.js'), 'module.exports = {"duck":"quack"};', cb);},
@@ -44,6 +45,9 @@ describe('hooks :: ', function() {
           if (err) { return callback(err); }
           assert.equal(sails.config.foo, 'bar');
           assert.equal(sails.config.abc, 123);
+          assert.equal(sails.config.horse, 'neigh');
+          assert.equal(sails.config.pig, 'oink');
+          assert.equal(sails.config.betty, 'spaghetti');
           assert.equal(typeof(sails.config.bar), 'undefined');
           return done();
         });
@@ -61,7 +65,7 @@ describe('hooks :: ', function() {
       var sails;
       before(function(done) {
         sails = Sails();
-        sails.load({hooks:{grunt:false}, dontFlattenConfig: true}, done);
+        sails.load({hooks:{grunt:false}}, done);
       });
 
       it('should load config from config/env/development.js', function() {
@@ -91,7 +95,7 @@ describe('hooks :: ', function() {
       var sails;
       before(function(done) {
         sails = Sails();
-        sails.load({hooks:{grunt:false}, dontFlattenConfig: true, environment: 'test-development'}, done);
+        sails.load({hooks:{grunt:false}, environment: 'test-development'}, done);
       });
 
       it('should load config from config/env/test-development.js', function() {
