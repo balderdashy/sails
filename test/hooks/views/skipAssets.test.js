0501468f21bb24b5782bc0d103230b6c4a215f8d
Scott Gress <scottmgress@gmail.com>
2016-10-17 15:38:11 -0500

    Add new anti-test for `skipAssets` (confirms that previous commit is working), and turn off Grunt for these tests.
    Hopefully fixes intermittent issue with skipAssets test failing, that was likely due to Grunt taking too long to get the sails.io.js file into the .tmp folder.

--- test/hooks/views/skipAssets.test.js
+++ test/hooks/views/skipAssets.test.js
@@ -15,6 +15,90 @@ var testSpawningSailsLiftChildProcessInCwd = require('../../helpers/test-spawnin
 
 describe('skipAssets', function() {
 
+  describe('Generate and lift a sails app which has a wildcard route WITHOUT using skipAssets', function() {
+
+    // Track the location of the Sails CLI, as well as the current working directory
+    // before we stop hopping about all over the place.
+    var originalCwd = process.cwd();
+    var pathToSailsCLI = path.resolve(__dirname, '../../../bin/sails.js');
+
+    // Track the location of the test app.
+    var pathToTestApp = path.resolve('testApp');
+
+    // Ensure test app does not already exist.
+    before(function (done) {
+      MFilesystem.rmrf({path: pathToTestApp}).exec(done);
+    });
+
+    // Create a Sails app on disk.
+    before(function (done){
+      MProcess.executeCommand({
+        command: util.format('node %s new %s', pathToSailsCLI, pathToTestApp),
+      }).exec(done);
+    });
+
+    // Change its routes file to use `skipAssets`.
+    before(function (done){
+      MFilesystem.write({
+        destination: path.join(pathToTestApp, 'config/routes.js'),
+        string: 'module.exports.routes = { \'get /*\': function(req, res) {return res.header(\'content-type\',\'text/plain\').send(\'some text\'); } };',
+        force: true,
+      }).exec(done);
+    });
+
+    // And CD in.
+    before(function (){
+      process.chdir(pathToTestApp);
+    });
+
+    // Lift the app using `sails lift` in the CWD,
+    // ensuring everything works as expected.
+    describe('running `sails lift', function (){
+      testSpawningSailsLiftChildProcessInCwd({
+        pathToSailsCLI: pathToSailsCLI,
+        liftCliArgs: ['--port=1331', '--hooks.grunt=false'],
+        httpRequestInstructions: {
+          method: 'GET',
+          uri: 'http://localhost:1331',
+        },
+        fnWithAdditionalTests: function (){
+          it('should return a view when requesting `http://localhost:1331/js/dependencies/sails.io.js`', function (done){
+            request({
+              method: 'GET',
+              uri: 'http://localhost:1331/js/dependencies/sails.io.js',
+            }, function(err, response, body) {
+              if (err) { return done(err); }
+              try {
+                if (!_.isString(response.headers['content-type'])) {
+                  return done(new Error('Expected a response content-type header when requesting an asset. `skipAssets` seems to be failing silently!'));
+                }
+                if (!response.headers['content-type'].match(/text\/plain/)) {
+                  return done(new Error('Expected text response content-type header when requesting an asset (but got `'+response.headers['content-type']+'`). `skipAssets` seems to be failing silently!'));
+                }
+                if (body !== 'some text') {
+                  return done(new Error('Expected text response an asset (but got `'+body+'`). `skipAssets` seems to be failing silently!'));
+                }
+              }
+              catch (e) { return done(e); }
+              return done();
+            });
+          });
+        }
+      });
+    });
+
+    // Finally clean up the Sails app we created earlier.
+    after(function (done) {
+      MFilesystem.rmrf({path: pathToTestApp}).exec(done);
+    });
+    // And CD back to where we were before.
+    after(function () {
+      process.chdir(originalCwd);
+    });
+
+
+  });
+
   describe('Generate and lift a sails app which has a wildcard route using skipAssets', function() {
 
     // Track the location of the Sails CLI, as well as the current working directory
@@ -41,7 +125,7 @@ describe('skipAssets', function() {
     before(function (done){
       MFilesystem.write({
         destination: path.join(pathToTestApp, 'config/routes.js'),
-        string: 'module.exports.routes = { \'get /*\': { view: \'homepage\', skipAssets: true } };',
+        string: 'module.exports.routes = { \'get /*\': { view: \'homepage\', skipAssets: true }, \'/js/dependencies/sails.io.js\': function(req,res){ return res.header(\'content-type\', \'application/javascript\').send(\'ok\'); } };',
         force: true,
       }).exec(done);
     });
@@ -56,7 +140,7 @@ describe('skipAssets', function() {
     describe('running `sails lift', function (){
       testSpawningSailsLiftChildProcessInCwd({
         pathToSailsCLI: pathToSailsCLI,
-        liftCliArgs: ['--port=1331'],
+        liftCliArgs: ['--port=1331', '--hooks.grunt=false'],
         httpRequestInstructions: {
           method: 'GET',
           uri: 'http://localhost:1331',
