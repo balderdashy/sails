9540fa22db55451878bbdb30ad4a80f9834f31fd
Scott Gress <scottmgress@gmail.com>
2016-10-20 18:30:48 -0500

    Change csrf-token-granting action identity to `security.grantcsrftoken`

--- test/integration/hook.csrf.test.js
+++ test/integration/hook.csrf.test.js
@@ -25,7 +25,7 @@ describe('CSRF ::', function() {
         hooks: {grunt: false, views: false, blueprints: false, policies: false},
         log: {level: 'error'},
         routes: {
-          '/csrfToken': {action: 'csrftoken'},
+          '/csrfToken': {action: 'security.grantcsrftoken'},
           'GET /viewtest/csrf': function(req, res) {res.send('csrf=' + res.locals._csrf);},
           'POST /user': function(req, res) {
             return res.send(201);
