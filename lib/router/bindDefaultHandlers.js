balderdashy/sails

commit 203720432e89f4d0b1a1797940b03f97546213ac

Bring https://github.com/balderdashy/sails/commit/cb74c3cd62d0b5e4ad5b672bb58e08a1e3cbcd52 onto master so it also applies in Sails v1.

lib/router/bindDefaultHandlers.js
@@ -20,42 +20,58 @@ module.exports = function(sails) {
      */
     500: function(err, req, res) {
 
-      // console.log('* * * FIRED FINAL HANDLER (500) * * *');
+      // console.log('* * * FIRED DEFAULT HANDLER (500) * * *');
       // console.log('args:',arguments);
-      // console.log('* * * </FIRED_FINAL_HANDLER_500> * * *');
+      // console.log('* * * </FIRED_DEFAULT_HANDLER_500> * * *');
       // console.log();
 
+      // First, check for special built-in errors from Express.
+      // We don't necessarily want to treat any error that is thrown with
+      // a `status` property of 400 as if it were intentional.  So we also check
+      // the error message.  In Express 5, hopefully this can be improved a bit
+      // further.
+      var msgMatches = err.message.match(/^Failed to decode param \'([^']+)\'/);
+      if (err.status === 400 && msgMatches) {
+        sails.log.verbose('Bad request: Could not decode the requested URL ('+req.path+')');
+        // Note for future: The problematic URL section is: `msgMatches[1]`
+        return res.send(400, 'Bad request: Could not decode requested URL.');
+      }
+
+      // Next, try to use `res.negotiate()`, if it exists and is valid.
       try {
-        // Use error handler if it exists
+
         if (typeof res.negotiate === 'function') {
-          // console.log('res.negotiate()');
           return res.negotiate(err);
-        }
-      } catch (e) {}
+        }//>-
 
+      } catch (e) { /* ignore any unexpected error encountered when attempting to respond w/ res.negotiate(). */ }
+
+      // Catch-all:
+      // Log a message and try to use `res.send` to respond.
       try {
 
-        // Catch-all:
-        // Log a message and try to use `res.send` to respond.
         sails.log.error('Server Error:');
         sails.log.error(err);
-        // console.log('catch-all.. log an error and send a 500');
-        if (process.env.NODE_ENV === 'production') { return res.send(500); }
-        return res.status(500).send(err);
-      }
+        if (process.env.NODE_ENV === 'production') {
+          return res.send(500);
+        }
+        else {
+          return res.send(500, err);
+        }
 
-      // Serious error occurred-- unable to send response.
-      //
-      // Note that in the future, we could also emit an `abort` message on the request object
-      // in this case-- then if an attached server is managing this request, it could monitor
-      // for `abort` events and manage its private resources (e.g. TCP sockets) accordingly.
-      // However, such contingencies should really handled by the underlying HTTP hook, so
-      // this might not actually make sense.
-      catch (errorSendingResponse) {
-        // console.log('serious error occurred');
+      } catch (errorSendingResponse) {
+
+        // Serious error occurred-- unable to send response.
+        //
+        // Note that in the future, we could also emit an `abort` message on the request object
+        // in this case-- then if an attached server is managing this request, it could monitor
+        // for `abort` events and manage its private resources (e.g. TCP sockets) accordingly.
+        // However, such contingencies should really handled by the underlying HTTP hook, so
+        // this might not actually make sense.
         sails.log.error('But no response could be sent because another error occurred:');
         sails.log.error(errorSendingResponse);
-      }
+
+      }//</catch>
     },
 
 

commit 132aaef5aa5635db308ae95fa72c07bbefc8c3c2

Fix for issue introduced in https://github.com/balderdashy/sails/commit/203720432e89f4d0b1a1797940b03f97546213ac

lib/router/bindDefaultHandlers.js
@@ -1,4 +1,10 @@
 /**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+
+/**
  * Default 500 and 404 handler.
  * (defers to res.serverError() and res.notFound() whenever possible)
  *
@@ -30,11 +36,16 @@ module.exports = function(sails) {
       // a `status` property of 400 as if it were intentional.  So we also check
       // the error message.  In Express 5, hopefully this can be improved a bit
       // further.
-      var msgMatches = err.message.match(/^Failed to decode param \'([^']+)\'/);
-      if (err.status === 400 && msgMatches) {
-        sails.log.verbose('Bad request: Could not decode the requested URL ('+req.path+')');
-        // Note for future: The problematic URL section is: `msgMatches[1]`
-        return res.send(400, 'Bad request: Could not decode requested URL.');
+
+      if (_.isError(err)) {
+
+        var msgMatches = err.message.match(/^Failed to decode param \'([^']+)\'/);
+        if (err.status === 400 && msgMatches) {
+          sails.log.verbose('Bad request: Could not decode the requested URL ('+req.path+')');
+          // Note for future: The problematic URL section is: `msgMatches[1]`
+          return res.send(400, 'Bad request: Could not decode requested URL.');
+        }
+
       }
 
       // Next, try to use `res.negotiate()`, if it exists and is valid.