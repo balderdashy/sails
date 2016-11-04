e30768dfa803c8c306ec063cbad67ccf16c120c1
Scott Gress <scottmgress@gmail.com>
2016-11-04 09:23:02 -0400

    Don't provide absolute path to `sails new` in skipAssets test

--- test/hooks/views/skipAssets.test.js
+++ test/hooks/views/skipAssets.test.js
@@ -34,7 +34,7 @@ describe('skipAssets', function() {
       pathToTestApp = path.resolve(tmpDir.name, 'testApp');
       // Create a new Sails app.
       MProcess.executeCommand({
-        command: util.format('node %s new %s', pathToSailsCLI, pathToTestApp),
+        command: util.format('node %s new %s', pathToSailsCLI, 'testApp'),
       }).exec(done);
     });
 
@@ -114,7 +114,7 @@ describe('skipAssets', function() {
       pathToTestApp = path.resolve(tmpDir.name, 'testApp');
       // Create a new Sails app.
       MProcess.executeCommand({
-        command: util.format('node %s new %s', pathToSailsCLI, pathToTestApp),
+        command: util.format('node %s new %s', pathToSailsCLI, 'testApp'),
       }).exec(done);
     });
 
