balderdashy/sails

commit 44c6d9b41e1b1ffd38150788085890c8c2cbe286

Update functionality of globals config
* The implicit default is `false` -- no globals at all
* If `sails.config.globals` is not `false`, it must be a dictionary such that:
  - If `sails.config.globals._` is not `false`, it must be an object
  - If `sails.config.globals.async` is not `false`, it must be an object
  - `sails.config.globals.models` must be a boolean
  - `sails.config.globals.sails` must be a boolean
* `sails.config.globals.services` still defaults to `true` if ``sails.config.globals` is a dictionary, because otherwise we have to talk about it explicitly and we're trying to phase services out.

lib/app/private/exposeGlobals.js
@@ -3,6 +3,7 @@
  */
 
 var _ = require('@sailshq/lodash');
+var flaverr = require('flaverr');
 var async = require('async');
 
 
@@ -18,8 +19,20 @@ var async = require('async');
 module.exports = function exposeGlobals() {
   var sails = this;
 
+  // Implicit default for globals is `false`.
+  // Note that this may not be what the default `config/globals.js`
+  // settings are.
+  if (_.isUndefined(sails.config.globals)) {
+    sails.config.globals = false;
+    return;
+  }
 
-  // Globals explicitly disabled
+  // If globals config is provided, it must be either `false` or a dictionary.
+  else if (sails.config.globals !== false && (!_.isObject(sails.config.globals) || _.isArray(sails.config.globals) || _.isFunction(sails.config.globals))) {
+    throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals` is defined, it must either be a dictionary or `false`'));
+  }
+
+  // Globals explicitly disabled.
   if (sails.config.globals === false) {
     sails.log.verbose('No global variables will be exposed.');
     return;
@@ -27,21 +40,39 @@ module.exports = function exposeGlobals() {
 
   sails.log.verbose('Exposing global variables... (you can customize/disable this by modifying the properties in `sails.config.globals`.  Set it to `false` to disable all globals.)');
 
-  sails.config.globals = sails.config.globals || {};
-
-  // Provide global access (if allowed in config)
+  // `sails.config.globals._` must be false or an object.
+  // (it's probably a function with lots of extra properties, but to future-proof, we'll allow any type of object)
   if (sails.config.globals._ !== false) {
-    global['_'] = _;
+    if (!_.isObject(sails.config.globals._)) {
+      throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals._` is defined, it must be either `false` or an object.'));
+    }
+    global['_'] = sails.config.globals._;
   }
+  // `sails.config.globals.async` must be false or an object.
+  // (it's probably a plain object aka dictionary, but to future-proof, we'll allow any type of object)
   if (sails.config.globals.async !== false) {
-    global['async'] = async;
+    if (!_.isObject(sails.config.globals.async)) {
+      throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals.async` is defined, it must be either `false` or an object.'));
+    }
+    global['async'] = sails.config.globals.async;
   }
+
+  // `sails.config.globals.sails` must be a boolean
   if (sails.config.globals.sails !== false) {
+    if (sails.config.globals.sails !== true) {
+      throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals` is defined, then `sails.config.globals.sails` must be a boolean.'));
+    }
     global['sails'] = sails;
   }
 
-  // `services` hook takes care of globalizing services (if enabled)
+  // `sails.config.globals.models` must be a boolean.
+  // `orm` hook takes care of actually globalizing models and adapters (if enabled)
+  if (sails.config.globals.models !== false && sails.config.globals.models !== true) {
+    throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals` is defined, then `sails.config.globals.models` must be a boolean.'));
+  }
 
-  // `orm` hook takes care of globalizing models and adapters (if enabled)
+  // `services` hook takes care of globalizing services (if enabled)
+  // It does this by default for now, so that we don't have to document configuring
+  // services, which we're trying to phase out in favor of helpers.
 
 };

test/hooks/views/res.render.i18n.js
@@ -38,7 +38,7 @@ describe('sails.hooks.views.render() with i18n', function (){
   before(function (done) {
     app = new Sails()
     .load({
-      globals: true,
+      globals: { sails: true, models: false, _: false, async: false, services: false } ,
       loadHooks: [
         'moduleloader',
         'userconfig',

test/integration/helpers/appHelper.js
@@ -241,7 +241,21 @@ module.exports = {
     _.each(deps, function(dep) {
       fs.ensureSymlinkSync(path.resolve(__dirname, '..', '..', '..', 'node_modules', dep), path.resolve(appPath, 'node_modules', dep));
     });
-  }
+  },
+
+  linkLodash: function(appPath) {
+    fs.ensureSymlinkSync(path.resolve(__dirname, '..', '..', '..', 'node_modules', '@sailshq', 'lodash'), path.resolve(appPath, 'node_modules', 'lodash'));
+  },
+
+
+  linkAsync: function(appPath) {
+    fs.ensureSymlinkSync(path.resolve(__dirname, '..', '..', '..', 'node_modules', 'async'), path.resolve(appPath, 'node_modules', 'async'));
+  },
+
+
+  linkSails: function(appPath) {
+    fs.ensureSymlinkSync(path.resolve(__dirname, '..', '..', '..'), path.resolve(appPath, 'node_modules', 'sails'));
+  },
 
 };
 

test/unit/app.registerAction.test.js
@@ -4,6 +4,7 @@
 
 var util = require('util');
 var assert = require('assert');
+var _ = require('@sailshq/lodash');
 
 var Sails = require('../../lib').constructor;
 

test/unit/app.reloadActions.test.js
@@ -37,6 +37,7 @@ describe('sails.reloadActions ::', function() {
 
       // Load the Sails app.
       (new Sails()).load({
+        globals: { sails: true, models: false, _: false, async: false, services: false },
         hooks: {
           grunt: false, views: false, blueprints: false, policies: false, pubsub: false, i18n: false,
           myHook: function() {return {initialize: function(cb) {this.registerActions(cb);}, registerActions: function(cb) {sails.registerAction(function(){}, 'custom-action'); return cb();}};}

commit 3e92c3a14f9300a7a9e14ec80443899372e2eb5b

fixes globals bug introduced in 44c6d9b

lib/app/private/exposeGlobals.js
@@ -43,7 +43,7 @@ module.exports = function exposeGlobals() {
   // `sails.config.globals._` must be false or an object.
   // (it's probably a function with lots of extra properties, but to future-proof, we'll allow any type of object)
   if (sails.config.globals._ !== false) {
-    if (!_.isObject(sails.config.globals._)) {
+    if (!_.isUndefined(sails.config.globals._) && !_.isObject(sails.config.globals._)) {
       throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals._` is defined, it must be either `false` or an object.'));
     }
     global['_'] = sails.config.globals._;
@@ -51,7 +51,7 @@ module.exports = function exposeGlobals() {
   // `sails.config.globals.async` must be false or an object.
   // (it's probably a plain object aka dictionary, but to future-proof, we'll allow any type of object)
   if (sails.config.globals.async !== false) {
-    if (!_.isObject(sails.config.globals.async)) {
+    if (!_.isUndefined(sails.config.globals.async) && !_.isObject(sails.config.globals.async)) {
       throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals.async` is defined, it must be either `false` or an object.'));
     }
     global['async'] = sails.config.globals.async;
@@ -59,7 +59,7 @@ module.exports = function exposeGlobals() {
 
   // `sails.config.globals.sails` must be a boolean
   if (sails.config.globals.sails !== false) {
-    if (sails.config.globals.sails !== true) {
+    if (!_.isUndefined(sails.config.globals.sails) && sails.config.globals.sails !== true) {
       throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals` is defined, then `sails.config.globals.sails` must be a boolean.'));
     }
     global['sails'] = sails;
@@ -67,7 +67,7 @@ module.exports = function exposeGlobals() {
 
   // `sails.config.globals.models` must be a boolean.
   // `orm` hook takes care of actually globalizing models and adapters (if enabled)
-  if (sails.config.globals.models !== false && sails.config.globals.models !== true) {
+  if (!_.isUndefined(sails.config.globals.models) && sails.config.globals.models !== false && sails.config.globals.models !== true) {
     throw flaverr('E_BAD_GLOBAL_CONFIG', new Error('If `sails.config.globals` is defined, then `sails.config.globals.models` must be a boolean.'));
   }
 