balderdashy/sails

commit b18c49dc73dbf9cb9616bf1fcd6c8684a00e1651

Move exposed 'generate()' to 'accessible/', and do the same thing for rc and loadGruntTasks.

accessible/generate.js
@@ -1,0 +1,24 @@
+/**
+ * Module dependencies
+ */
+
+var sailsgen = require('sails-generate');
+
+
+/**
+ * require('sails/accessible/generate')
+ *
+ * Generate files or folders.
+ *
+ * > This is an exposed version of sails-generate for programmatic use.
+ * > (available on `require('sails').Sails.generate()`)
+ *
+ * @param {Dictionary} scope
+ * @param {Function|Dictionary} cbOrHandlers
+ */
+SailsFactory.generate = function (){
+
+  return sailsgen.apply(this, Array.prototype.slice.call(arguments));
+
+};
+

accessible/load-grunt-tasks.js
@@ -1,0 +1,71 @@
+/**
+ * Module dependencies
+ */
+
+var path = require('path');
+var includeAll = require('include-all');
+
+
+
+/**
+ * require('sails/accessible/load-grunt-tasks')
+ *
+ * Load Grunt tasks from the conventional paths using the given
+ * `grunt` instance.
+ *
+ * > This should not be modified.
+ * > It's job is to eliminate the need for an extra `include-all` dep.
+ * > in userland just to load Grunt tasks.  (The default Gruntfile.js
+ * > calls this function directly-- this is partially to make it easy
+ * > to do any major customizations directly in that file.  But more
+ * > importantly, it's because Grunt doesn't work without it.)
+ *
+ * @param {String} appPath
+ * @param {Ref} grunt
+ */
+module.exports = function loadGruntTasks (appPath, grunt){
+
+  // Load JavaScript files from `tasks/config/**/*` and `tasks/register/**/*`
+  // ========================================================
+  // Load Grunt configuration modules from the specified
+  // relative path. These modules should export a function
+  // that, when run, should either load/configure or register
+  // a Grunt task.
+  //
+  // This uses the include-all library in order to require all of
+  // the app's grunt configurations and task registrations dynamically.
+  var helperTasks = includeAll({
+    dirname: path.resolve(appPath, './tasks/config'),
+    filter: /(.+)\.js$/,
+    excludeDirs: /^\.(git|svn)$/
+  }) || {};
+  // Same thing for our main tasklists.
+  var mainTasks = includeAll({
+    dirname: path.resolve(appPath, './tasks/register'),
+    filter: /(.+)\.js$/,
+    excludeDirs: /^\.(git|svn)$/
+  }) || {};
+
+
+  // Ensure that a default task exists.
+  // ========================================================
+  if (!mainTasks.default) {
+    mainTasks.default = function(grunt) {
+      grunt.registerTask('default', []);
+    };
+  }
+
+
+  // Run task functions to configure Grunt.
+  // ========================================================
+  // Invoke the function from each Grunt configuration module
+  // with a single argument - the `grunt` object.
+  Object.keys(helperTasks).forEach(function (taskName){
+    helperTasks[taskName](grunt);
+  });
+  // Same thing for our main tasklists.
+  Object.keys(mainTasks).forEach(function (taskName){
+    mainTasks[taskName](grunt);
+  });
+
+};

accessible/rc.js
@@ -1,0 +1,19 @@
+/**
+ * Module dependencies
+ */
+
+var rc = require('rc');
+
+
+/**
+ * require('sails/accessible/rc')
+ *
+ * A direct reference to Sails' built-in `rc` dependency.
+ *
+ * > This should not be modified.
+ * > It's job is to eliminate the need for an extra `rc` dep. in userland
+ * > just to load cmdline config in app.js.
+ *
+ * @type {Ref}
+ */
+module.exports = rc;

lib/app/Sails.js
@@ -160,5 +160,8 @@ Sails.prototype.initializeController = require('./private/controller');
 // (may be deprecated in a future release)
 Sails.prototype.util = require('sails-util');
 
+
+
+
 // Expose Sails constructor
 module.exports = Sails;

lib/app/index.js
@@ -8,7 +8,7 @@ var _ = require('lodash');
 
 
 /**
- * Expose `Sails` factory
+ * Expose `Sails` factory...thing.
  * (maintains backwards compatibility w/ constructor usage)
  */
 
@@ -23,3 +23,6 @@ function SailsFactory() {
 var singleton = SailsFactory();
 SailsFactory.isLocalSailsValid = _.bind(singleton.isLocalSailsValid, singleton);
 SailsFactory.isSailsAppSync = _.bind(singleton.isSailsAppSync, singleton);
+
+
+

lib/index.js
@@ -10,10 +10,8 @@ var Sails = require('./app');
 // (maintains legacy support)
 module.exports = new Sails();
 
-// Expose constructor for convenience/tests
-module.exports.Sails = Sails;
-
-
+// Expose constructor as `.Sails` for convenience/tests:
+// =========================================================
 // To access the Sails app constructor, do:
 // var Sails = require('sails').constructor;
 // or:
@@ -21,27 +19,6 @@ module.exports.Sails = Sails;
 //
 // Then:
 // var newApp = new Sails();
+// =========================================================
+module.exports.Sails = Sails;
 
-
-
-
-/**
- * Sails.generate()
- *
- * Generate files or folders.
- *
- * >This is an exposed version of sails-generate for programmatic use.
- * > (available on `require('sails').Sails.generate()`)
- *
- * @param {Dictionary} scope
- * @param {Function} cb
- */
-module.exports.Sails.generate = function (){
-
-  // Wait until this function is actually called to do this require.
-  // (no reason to slow down production lifts)
-  var sailsgen = require('sails-generate');
-
-  return sailsgen.apply(this, Array.prototype.slice.call(arguments));
-
-};

commit 35de04576b629adece1ef47255f108129b2218ce

Fix problem surfaced by eslint (originally introduced in https://github.com/balderdashy/sails/commit/b18c49dc73dbf9cb9616bf1fcd6c8684a00e1651)

accessible/generate.js
@@ -16,7 +16,7 @@ var sailsgen = require('sails-generate');
  * @param {Dictionary} scope
  * @param {Function|Dictionary} cbOrHandlers
  */
-SailsFactory.generate = function (){
+module.exports = function generate (){
 
   return sailsgen.apply(this, Array.prototype.slice.call(arguments));
 