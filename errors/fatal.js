balderdashy/sails

commit 4ce7ddf405804f7534085b8230c3083bcc31d3e3

Better notes on terminateProcess.

errors/fatal.js
@@ -44,6 +44,24 @@ module.exports = {
 		_terminateProcess(1);
 	},
 
+	// TODO: replace the inline version of this error
+	// app/loadHooks.js:42
+	malformedHook: function () {
+		log.error('Malformed hook! (' + id + ')');
+		log.error('Hooks should be a function with one argument (`sails`)');
+		_terminateProcess(1);
+	},
+
+	// TODO: replace the inline version of this error
+	// app/load.js:146
+	hooksTookTooLong: function () {
+		var hooksTookTooLongErr = 'Hooks are taking way too long to get ready...  ' +
+			'Something is amiss.\nAre you using any custom hooks?\nIf so, make sure the hook\'s ' +
+			'`initialize()` method is triggering it\'s callback.';
+		log.error(hooksTookTooLongErr);
+		process.exit(1);
+	},
+
 
 
 	// Invalid user module errors
@@ -133,18 +151,52 @@ module.exports = {
 
 
 
+
 /**
- * Terminate the process as elegantly as possible.
- * If process.env is 'test', emit a mock signal instead.
+ * 
+ * TODO: Make all of this more elegant.
+ * ========================================================
+ * + Ideally we don't call `process.exit()` at all.
+ * We should consistently use `sails.lower()` for unhandleable core
+ * errors and just trigger the appropriate callback w/ an error for
+ * core lift/load and any CLI errors.
+ * 
+ * + Then we won't have to worry as much about dangling child processes
+ * and things like that. Plus it's more testable that way.
+ * 
+ * In practice, the best way to do this may be an error domain or an
+ * event emitted on the sails object (or both!)
+ * ========================================================
+ *
+ *
+ * 
+ * TODO: Merge w/ app/teardown.js
+ * ========================================================
+ * (probably achievable by doing the aforementioned cleanup)
+ * ========================================================
  */
-function _terminateProcess (code) {
+
+
+
+/**
+ * _terminateProcess
+ *
+ * Terminate the process as elegantly as possible.
+ * If process.env is 'test', throw instead.
+ * 
+ * @param  {[type]} code [console error code]
+ * @param  {[type]} opts [currently unused]
+ */
+function _terminateProcess (code, opts) {
 	if ( process.env.NODE_ENV === 'test' ) {
 		var Signal = new Error({
 			type: 'terminate',
-			code: code
+			code: code,
+			options: { todo: 'put the stuff from the original errors in here' }
 		});
-		process.emit('_sails', Signal);
 		throw Signal;
 	}
+
 	return process.exit(code);
 }
+

test/unit/app.initializeHooks.test.js
@@ -60,13 +60,37 @@ describe('app.initializeHooks()', function() {
 
 
 	describe('configured with a hook (`noop2`), but not its dependency (`noop`)', function () {
-		$Sails.load({
+		$Sails.load
+		.expectTerminatedProcess({
 			hooks: {
+
+				// This forced failure is only temporary--
+				// very hard to test right now as things stand.
+				whadga: function (sails) {
+					throw 'forced failure';
+				},
+
 				noop2: customHooks.NOOP2
 			}
 		});
+	});
 
-		it('should throw a fatal error');
+
+
+	describe('configured with a hook that throws', function () {
+		$Sails.load
+		.expectTerminatedProcess({
+			hooks: {
+
+				// This forced failure is only temporary--
+				// very hard to test right now as things stand.
+				whadga: function (sails) {
+					throw 'forced failure';
+				},
+
+				noop2: customHooks.NOOP2
+			}
+		});
 	});
 
 

test/unit/helpers/sails.js
@@ -2,6 +2,9 @@
  * Module dependencies
  */
 var _ = require('lodash');
+var util = require('util');
+var should = require('should');
+var domain = require('domain');
 var Sails = require('root-require')('lib/app');
 
 /**
@@ -16,35 +19,78 @@ var helper = {
 	 * Can call:
 	 *	-> helper.load()
 	 *	-> helper.load.withAllHooksDisabled()
+	 *	-> helper.load.expectingTerminatedProcess()
 	 */
 	load: (function () {
 
-		var testDefaults = { log: {level: 'error'} };
+		/**
+		 * _cleanOptions()
+		 * 
+		 * @param {Object} options
+		 * @type {Function}
+		 * @api private
+		 */
+		function _cleanOptions (options) {
+			var testDefaults = { log: {level: 'error'} };
+			options = _.isObject(options) ? options : {};
+			return _.defaults(options, testDefaults);
+		}
+
+
 
 		var _load = function (options) {
 			
+			var testDescription, msSlowThreshold;
+			var sailsOpts = _cleanOptions(options);
+
 			// Defaults
 			// (except use test defaults)
-			if (!options) {
-				_with('default settings', testDefaults, 750);
-				return;
+			if (!_.isObject(options)) {
+				testDescription = 'default settings';
+				msSlowThreshold = 750;
+			}
+			else {
+				// Specified options + defaults
+				// (except default log level to 'error')
+				testDescription = util.inspect(options);
+				msSlowThreshold = 2000;
 			}
 
-			// Specified options + defaults
-			// (except default log level to 'error')
-			var humanReadableOpts = require('util').inspect(options);
-			_with(humanReadableOpts,
-				_.defaults(options, testDefaults),
-				2000);
 
+			return _with(testDescription, sailsOpts, msSlowThreshold);
 		};
 
 		_load.withAllHooksDisabled = function () {
-			_with('all hooks disabled', {
+			return _with('all hooks disabled', {
 				log: {level: 'error'},
 				globals: false,
 				loadHooks: []
 			}, 500);
+		};
+
+		_load.expectTerminatedProcess = function( options ) {
+			options = _.isObject(options) ? options : {};
+			var sailsOpts = _cleanOptions(options);
+
+			it(', sails should deliberately terminate process', function (done) {
+				var sails = new Sails();
+				
+				// Use error domain to catch failure
+				var DELIBERATE_ERROR = domain.create();
+				DELIBERATE_ERROR.on('error', function (err) {
+					return done();
+				});
+				DELIBERATE_ERROR.run(function () {
+					sails.load(sailsOpts || {}, function (err) {
+						var e = 
+						'Should not have made it to load() ' +
+						'callback, with or without an error!';
+						if (err) e+='\nError: ' + util.inspect(err);
+						return done(new Error(e));
+					});
+				});
+
+			});
 		};
 
 		return _load;
@@ -78,7 +124,8 @@ module.exports = helper;
  * @param  {String} description
  * @param  {Object} sailsOpts
  * @param  {Integer} msThreshold [before we consider it "slow"]
- * 
+ *
+ * @returns {Chainable}
  * @api private
  */
 function _with (description, sailsOpts, msThreshold) {
@@ -97,3 +144,5 @@ function _with (description, sailsOpts, msThreshold) {
 	});
 
 }
+
+

commit 0a8b7d58c32bb4cfc5101cbe413d3ff5ec66db93

One last fix for an old bug surfaced by eslint (introduced in https://github.com/balderdashy/sails/commit/4ce7ddf405804f7534085b8230c3083bcc31d3e3#diff-f1627914693a8027e9d83a235de0ff9fR50)

errors/fatal.js
@@ -49,14 +49,14 @@ module.exports = {
       'has a corrupted, missing, or un-parsable package.json file.'
     );
     log.error('You may consider running:');
-    log.error('rm -rf ' + pathTo_localSails + ' && npm install sails@' + app.dependencies.sails);
+    log.error('rm -rf ' + pathTo_localSails + ' && npm install sails@' + requiredVersion);
     _terminateProcess(1);
   },
 
   // FUTURE: inline this error
   // app/loadHooks.js:42
   malformedHook: function() {
-    log.error('Malformed hook! (' + id + ')');
+    log.error('Malformed hook!');
     log.error('Hooks should be a function with one argument (`sails`)');
     _terminateProcess(1);
   },